5cccd43883a3c2005a72694a56d7463238029668
zhangyixing1007 <41513796+zhangyixing1007@users.noreply.github.com>
2019-09-25 16:43:01 +0800

    Update Solution.java
    /*
    LeetCode Problem No.226:    https://leetcode.com/problems/invert-binary-tree/
    Author:                     zhangyixing1007
    Idea:                       recursive
    Time:                       1 ms, beat 88.75%
    Space:                      35.5MB, beat 32.81%
    */

--- 226_invertTree/Solution.java
+++ 226_invertTree/Solution.java
@@ -1,3 +1,21 @@
+/*
+LeetCode Problem No.226:    https://leetcode.com/problems/invert-binary-tree/
+Author:                     zhangyixing1007
+Idea:                       recursive
+Time:                       1 ms, beat 88.75%
+Space:                      35.5MB, beat 32.81%
+*/
+
+/**
+* Definition for a binary tree node.
+* public class TreeNode {
+*     int val;
+*     TreeNode left;
+*     TreeNode right;
+*     TreeNode(int x) { val = x; }
+* }
+*/
+
 class Solution {
     public TreeNode invertTree(TreeNode root) {
         if (root==null) return null;
