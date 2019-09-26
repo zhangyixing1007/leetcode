fb411df403ff3dc6e20ba6d5344d7f3f4fa4abb5
zhangyixing1007 <41513796+zhangyixing1007@users.noreply.github.com>
2019-09-26 13:19:48 +0800

    Create Solution.java
    /*
    LeetCode Problem No.70:     https://leetcode.com/problems/climbing-stairs/
    Author:                     zhangyixing1007
    Idea:                       iteration or recursion both okay, very classical 
    Time:                       0 ms, beat 100%
    Space:                      32.8MB, beat 74.43%
    */

---
+++ 070_climbStairs/Solution.java
@@ -0,0 +1,21 @@
+/*
+LeetCode Problem No.70:     https://leetcode.com/problems/climbing-stairs/
+Author:                     zhangyixing1007
+Idea:                       iteration or recursion both okay, very classical 
+Time:                       0 ms, beat 100%
+Space:                      32.8MB, beat 74.43%
+*/
+
+
+class Solution {
+    public int climbStairs(int n) {
+        if (n<4) return n;
+        
+        int[] methods = new int[n+1];
+        methods[1] = 1;
+        methods[2] = 2;
+        for (int i = 3; i<methods.length; i++)
+        {methods[i] = methods[i-1] + methods[i-2];}
+        return methods[n];        
+    }
+}
