5d99bb1d335daa52d08a6aa51649ddb2ebbb3040
zhangyixing1007 <41513796+zhangyixing1007@users.noreply.github.com>
2019-09-26 12:16:25 +0800

    Create Solution.java
    /*
    LeetCode Problem No.226:    https://leetcode.com/problems/two-sum/
    Author:                     zhangyixing1007
    Idea:                       loop once save values in HashMap, and check its complement
    Time:                       3 ms, beat 99.12%
    Space:                      37.8MB, beat 82.40%
    */

---
+++ 001_twoSum/Solution.java
@@ -0,0 +1,23 @@
+/*
+LeetCode Problem No.226:    https://leetcode.com/problems/two-sum/
+Author:                     zhangyixing1007
+Idea:                       loop once save values in HashMap, and check its complement
+Time:                       3 ms, beat 99.12%
+Space:                      37.8MB, beat 82.40%
+*/
+
+import java.util.HashMap;
+
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+    Map<Integer,Integer> map = new HashMap<Integer,Integer>();
+        for (int i = 0; i < nums.length; i ++){
+            int complement = target - nums[i];
+            if (map.containsKey(complement) && map.get(complement) != i){
+                return new int[] {map.get(complement),i};
+            }
+            map.put(nums[i],i);
+        }
+        return new int[]{};
+    }
+}
