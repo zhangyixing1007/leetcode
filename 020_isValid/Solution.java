91c2c6266b3e9d23d07b264a5a31b417d477020f
zhangyixing1007 <41513796+zhangyixing1007@users.noreply.github.com>
2019-09-26 12:40:02 +0800

    Create Solution.java
    /*
    LeetCode Problem No.226:    https://leetcode.com/problems/valid-parentheses/
    Author:                     zhangyixing1007
    Idea:                       push "(" "{" "[" into stack , and while ")" "}" "]", check if top is "(" "{" "[" correspondingly
    Time:                       2 ms, beat 97.54%
    Space:                      34.1MB, beat 87.48%
    */

---
+++ 020_isValid/Solution.java
@@ -0,0 +1,32 @@
+/*
+LeetCode Problem No.226:    https://leetcode.com/problems/valid-parentheses/
+Author:                     zhangyixing1007
+Idea:                       push "(" "{" "[" into stack , and while ")" "}" "]", check if top is "(" "{" "[" correspondingly
+Time:                       2 ms, beat 97.54%
+Space:                      34.1MB, beat 87.48%
+*/
+
+import java.util.Stack;
+
+class Solution {
+    public boolean isValid(String s) {
+        Stack<Character> bracket = new Stack<Character>();
+        for (int i = 0; i < s.length(); i++){
+            char c = s.charAt(i);
+            if (c == '('|| c == '{' || c == '[')
+            {bracket.push(c);}
+            else{
+                if (bracket.empty()==true) return false;
+                char t = bracket.pop();
+                if (c == ')'&& t!='(')
+                {return false;}
+                else if (c == ']'&& t!='[')
+                {return false;}
+                else if (c == '}'&& t!='{')
+                {return false;}                 
+            }       
+        }
+        if(bracket.empty()==true) return true;
+        else return false;
+    }
+}
