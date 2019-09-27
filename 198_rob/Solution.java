ab20b899eaddbeedf71219b0aa9f61378807b4da
zhangyixing1007 <41513796+zhangyixing1007@users.noreply.github.com>
2019-09-27 13:59:32 +0800

    Create Solution.java
    /*
    LeetCode Problem No.198:    https://leetcode.com/problems/house-robber/
    Author:                     zhangyixing1007
    Idea:                       recursion
    Time:                       4 ms, beat 70.21%
    Space:                      41.9MB, beat 83.65%
    */

---
+++ 198_rob/Solution.java
@@ -0,0 +1,21 @@
+/*
+LeetCode Problem No.198:    https://leetcode.com/problems/house-robber/
+Author:                     zhangyixing1007
+Idea:                       recursion
+Time:                       4 ms, beat 70.21%
+Space:                      41.9MB, beat 83.65%
+*/
+
+class Solution { 
+    public int rob(int[] nums) { 
+        int first = 0; 
+        int second = 0; 
+        for (int num:nums) 
+        { 
+            int tmp = second; 
+            second = Math.max(first+num,second); 
+            first = tmp; 
+        } 
+        return second; 
+    } 
+} 
