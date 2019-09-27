7716aabcebb2da2b9d399876aa5becdcbaff0fa5
zhangyixing1007 <41513796+zhangyixing1007@users.noreply.github.com>
2019-09-27 14:11:48 +0800

    Create Solution.java
    /*
    LeetCode Problem No.283:    https://leetcode.com/problems/move-zeroes/
    Author:                     zhangyixing1007
    Idea:                       move nums[i] to the place it should be by exchanging until that place has been occupied correctly
    Time:                       0 ms, beat 100%
    Space:                      37.3MB, beat 95.82%
    */

---
+++ 283_moveZeroes/Solution.java
@@ -0,0 +1,21 @@
+/*
+LeetCode Problem No.283:    https://leetcode.com/problems/move-zeroes/
+Author:                     zhangyixing1007
+Idea:                       move nums[i] to the place it should be by exchanging until that place has been occupied correctly
+Time:                       0 ms, beat 100%
+Space:                      37.3MB, beat 95.82%
+*/
+
+class Solution { 
+    public void moveZeroes(int[] nums) { 
+        for (int i = 0, j = 0; i < nums.length; i++) 
+        { 
+            if(nums[i]!=0) 
+            { 
+                if(i!=j) 
+                {int tmp = nums[i];nums[i] = nums[j];nums[j] = tmp;}                
+                j++; 
+            } 
+        } 
+    } 
+} 
