fd5172e8405820d990cf9a4878760ad20170144a
zhangyixing1007 <41513796+zhangyixing1007@users.noreply.github.com>
2019-09-27 14:03:22 +0800

    Create Solution.java
    /*
    LeetCode Problem No.206:    https://leetcode.com/problems/reverse-linked-list/
    Author:                     zhangyixing1007
    Idea:                       fast/slow pointers, pay attention to odd/even case
    Time:                       0 ms, beat 100%
    Space:                      36.7MB, beat 54.36%
    */

---
+++ 206_reverseList/Solution.java
@@ -0,0 +1,22 @@
+/*
+LeetCode Problem No.206:    https://leetcode.com/problems/reverse-linked-list/
+Author:                     zhangyixing1007
+Idea:                       fast/slow pointers, pay attention to odd/even case
+Time:                       0 ms, beat 100%
+Space:                      36.7MB, beat 54.36%
+*/
+
+class Solution { 
+    public ListNode reverseList(ListNode head) { 
+        if (head == null) return null; 
+         
+        ListNode prev = null, curr = head, next = head.next;         
+        while(next!=null) 
+        { 
+            curr.next = prev; 
+            prev = curr; curr = next; next = next.next; 
+        } 
+        curr.next = prev; 
+        return curr; 
+    } 
+} 
