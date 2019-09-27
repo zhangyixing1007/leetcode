4bfa7e8553e2e90e567e24078768ebd61802ce37
zhangyixing1007 <41513796+zhangyixing1007@users.noreply.github.com>
2019-09-27 14:52:41 +0800

    Create Solution.java
    /*
    LeetCode Problem No.617:    https://leetcode.com/problems/merge-two-binary-trees/
    Author:                     zhangyixing1007
    Idea:                       loop once save values in HashMap, and check its complement
    Time:                       1 ms, beat 99.91%
    Space:                      40.3 MB, beat 95.37%
    */

---
+++ 617_mergeTrees/Solution.java
@@ -0,0 +1,28 @@
+/*
+LeetCode Problem No.617:    https://leetcode.com/problems/merge-two-binary-trees/
+Author:                     zhangyixing1007
+Idea:                       loop once save values in HashMap, and check its complement
+Time:                       1 ms, beat 99.91%
+Space:                      40.3 MB, beat 95.37%
+*/
+
+/** 
+ * Definition for a binary tree node. 
+ * public class TreeNode { 
+ *     int val; 
+ *     TreeNode left; 
+ *     TreeNode right; 
+ *     TreeNode(int x) { val = x; } 
+ * } 
+ */ 
+class Solution { 
+    public TreeNode mergeTrees(TreeNode t1, TreeNode t2) { 
+        if (t1==null&&t2==null) return null; 
+        if (t1==null&&t2!=null) return t2; 
+        if (t1!=null&&t2==null) return t1; 
+        t1.val += t2.val; 
+        t1.left = mergeTrees(t1.left,t2.left); 
+        t1.right = mergeTrees(t1.right,t2.right); 
+        return t1; 
+    } 
+} 
