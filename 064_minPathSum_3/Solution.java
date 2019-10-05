14abd96b6f99361d57fec82c583e2eebda50ee43
zhangyixing1007 <41513796+zhangyixing1007@users.noreply.github.com>
2019-10-06 00:01:34 +0800

    Create Solution.java
    /*
    LeetCode Problem No.64:     https://leetcode.com/problems/minimum-path-sum/
    Author:                     zhangyixing1007
    Idea:                       2-fold iteration, 1-D array as intermediate saving
    Time:                       2 ms, beat 99.72%
    Space:                      42.2 MB, beat 44.00%
    */

---
+++ 064_minPathSum_3/Solution.java
@@ -0,0 +1,31 @@
+/*
+LeetCode Problem No.64:     https://leetcode.com/problems/minimum-path-sum/
+Author:                     zhangyixing1007
+Idea:                       2-fold iteration, 1-D array as intermediate saving
+Time:                       2 ms, beat 99.72%
+Space:                      42.2 MB, beat 44.00%
+*/
+
+class Solution {
+    public int minPathSum(int[][] grid) {
+        int m = grid.length;
+        int n = grid[0].length;
+        if (m==0||n==0) return 0;
+        
+        int[] sum = new int[n];
+        System.arraycopy(grid[0],0,sum,0,n);
+        for (int j = 1; j < n; j++)
+        {
+            sum[j] += sum[j-1];
+        }
+        for (int i = 1; i < m; i++)
+        {
+            sum[0] = sum[0] + grid[i][0];
+            for (int j = 1; j < n; j++)
+            {
+                sum[j] = sum[j-1]<sum[j]? (sum[j-1] + grid[i][j]) : (sum[j] + grid[i][j]);
+            }
+        }
+        return sum[n-1];
+    }
+}
