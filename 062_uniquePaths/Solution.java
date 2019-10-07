7ec4a79894b7dd5845ca37ea5cc65295e6e162a6
zhangyixing1007 <41513796+zhangyixing1007@users.noreply.github.com>
2019-10-07 23:41:14 +0800

    Create Solution.java
    /*
    LeetCode Problem No.62:     https://leetcode.com/problems/unique-paths/
    Author:                     zhangyixing1007
    Idea:                       simply calculate by formula, use long
    Time:                       0 ms, beat 100%
    Space:                      33.1 MB, beat 13.92%
    */

---
+++ 062_uniquePaths/Solution.java
@@ -0,0 +1,23 @@
+/*
+LeetCode Problem No.62:     https://leetcode.com/problems/unique-paths/
+Author:                     zhangyixing1007
+Idea:                       simply calculate by formula, use long
+Time:                       0 ms, beat 100%
+Space:                      33.1 MB, beat 13.92%
+*/
+
+class Solution {
+    public int uniquePaths(int m, int n) {
+        if (m==0||n==0) return 0;
+        long result = 1;
+        int count = m<n? m-1:n-1;
+        int i = 1;
+        while (i<=count)
+        {
+            result *= m + n - 1 - i;
+            result /= i;
+            i ++;    
+        }
+        return (int) result;
+    }
+}
