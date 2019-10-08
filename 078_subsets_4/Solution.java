430304eeb601793b3a953d5f934abc9e420545ab
zhangyixing1007 <41513796+zhangyixing1007@users.noreply.github.com>
2019-10-08 21:25:13 +0800

    Create Solution.java
    /*
    LeetCode Problem No.78:     https://leetcode.com/problems/subsets/
    Author:                     zhangyixing1007
    Idea:                       bit operation
    Time:                       1 ms, beat 99.81%
    Space:                      36.9 MB, beat 45.19%
    */

---
+++ 078_subsets_4/Solution.java
@@ -0,0 +1,25 @@
+/*
+LeetCode Problem No.78:     https://leetcode.com/problems/subsets/
+Author:                     zhangyixing1007
+Idea:                       bit operation
+Time:                       1 ms, beat 99.81%
+Space:                      36.9 MB, beat 45.19%
+*/
+
+class Solution {
+    public List<List<Integer>> subsets(int[] nums)
+    {
+        List<List<Integer>> list = new ArrayList<>();
+        for (int i = 0; i < 1 << nums.length; i++)
+        {
+            List<Integer> subset = new ArrayList<>();
+            for (int j = 0; j < nums.length; j++)
+            {
+                if (((i>>j)&1)==1) 
+                    subset.add(nums[j]);
+            }
+            list.add(subset);
+        }
+        return list;
+    }
+}
