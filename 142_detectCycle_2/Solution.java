6170c6f6e305c2dbd423232317245691c67d69a7
zhangyixing1007 <41513796+zhangyixing1007@users.noreply.github.com>
2019-10-10 18:33:06 +0800

    Create Solution.java
    /*
    LeetCode Problem No.142:    https://leetcode.com/problems/linked-list-cycle-ii/
    Author:                     zhangyixing1007
    Idea:                       two pointers to check loop, and then Floyd Algorithm 
    Time:                       0 ms, beat 100%
    Space:                      36.1 MB, beat 5.03%
    */

---
+++ 142_detectCycle_2/Solution.java
@@ -0,0 +1,42 @@
+/*
+LeetCode Problem No.142:    https://leetcode.com/problems/linked-list-cycle-ii/
+Author:                     zhangyixing1007
+Idea:                       two pointers to check loop, and then Floyd Algorithm 
+Time:                       0 ms, beat 100%
+Space:                      36.1 MB, beat 5.03%
+*/
+
+/**
+* Definition for singly-linked list.
+* class ListNode {
+*     int val;
+*     ListNode next;
+*     ListNode(int x) {
+*         val = x;
+*         next = null;
+*     }
+* }
+*/
+
+public class Solution
+{
+    public ListNode detectCycle(ListNode head)
+    {
+        ListNode fast = head;
+        ListNode slow = head;
+        while(fast!=null&&fast.next!=null)
+        {
+            fast = fast.next.next;
+            slow = slow.next;
+            if (fast == slow) break;
+        }
+        if (fast==null||fast.next==null) return null;
+        slow = head;
+        while (fast!=slow)
+        {
+            fast = fast.next;
+            slow = slow.next;
+        }
+        return slow;
+    }
+}
