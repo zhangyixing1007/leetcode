c37a4fbad06b4771e7aaaa1a6985f05652535502
zhangyixing1007 <41513796+zhangyixing1007@users.noreply.github.com>
2019-10-16 13:24:43 +0800

    Create Solution.java
    /*
    LeetCode Problem No.215:    https://leetcode.com/problems/kth-largest-element-in-an-array/
    Author:                     zhangyixing1007
    Idea:                       use PriorityQueue
    Time:                       6 ms, beat 29.16%
    Space:                      88.65 MB, beat 94.18%
    */

---
+++ 215_findKthLargest_2/Solution.java
@@ -0,0 +1,21 @@
+/*
+LeetCode Problem No.215:    https://leetcode.com/problems/kth-largest-element-in-an-array/
+Author:                     zhangyixing1007
+Idea:                       use PriorityQueue
+Time:                       6 ms, beat 29.16%
+Space:                      88.65 MB, beat 94.18%
+*/
+
+class Solution 
+{
+    public int findKthLargest(int[] nums, int k) 
+    {
+        PriorityQueue<Integer> q = new PriorityQueue<>((n1,n2)->n1-n2);
+        for (int num:nums)
+        {
+            q.add(num);
+            if (q.size()>k) q.poll();
+        }
+        return q.poll();
+    }
+}
