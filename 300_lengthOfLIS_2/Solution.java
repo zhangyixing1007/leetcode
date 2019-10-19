f61985dba5a4144bf0b3ea56a76df872c77a364c
zhangyixing1007 <41513796+zhangyixing1007@users.noreply.github.com>
2019-10-19 09:24:08 +0800

    Create Solution.java
    /*
    LeetCode Problem No.3oo:    https://leetcode.com/problems/longest-increasing-subsequence/
    Author:                     zhangyixing1007
    Idea:                       dynamic programming
    Time:                       1 ms, beat 99.26%
    Space:                      35.8 MB, beat 35.10%
    */

---
+++ 300_lengthOfLIS_2/Solution.java
@@ -0,0 +1,24 @@
+/*
+LeetCode Problem No.3oo:    https://leetcode.com/problems/longest-increasing-subsequence/
+Author:                     zhangyixing1007
+Idea:                       dynamic programming
+Time:                       1 ms, beat 99.26%
+Space:                      35.8 MB, beat 35.10%
+*/
+
+class Solution 
+{
+    public int lengthOfLIS(int[] nums)
+    {
+        int [] dp = new int[nums.length];
+        int len = 0;
+        for (int num : nums)
+        {
+            int i = Arrays.binarySearch(dp,0,len,num);
+            if (i < 0) i = -(i+1);
+            dp[i] = num;
+            if (i == len) len++;
+        }
+        return len;
+    }
+}
