fe68896ee8883b04e20ccba5a6eb7521f84a4399
zhangyixing1007 <41513796+zhangyixing1007@users.noreply.github.com>
2019-10-19 09:29:04 +0800

    Update Solution.java
    /*
    LeetCode Problem No.300:    https://leetcode.com/problems/longest-increasing-subsequence/
    Author:                     zhangyixing1007
    Idea:                       dynamic programming + binary search, O(nlogn)
    Time:                       1 ms, beat 99.26%
    Space:                      35.8 MB, beat 35.10%
    */

--- 300_lengthOfLIS_2/Solution.java
+++ 300_lengthOfLIS_2/Solution.java
@@ -1,7 +1,7 @@
 /*
-LeetCode Problem No.3oo:    https://leetcode.com/problems/longest-increasing-subsequence/
+LeetCode Problem No.300:    https://leetcode.com/problems/longest-increasing-subsequence/
 Author:                     zhangyixing1007
-Idea:                       dynamic programming
+Idea:                       dynamic programming + binary search, O(nlogn)
 Time:                       1 ms, beat 99.26%
 Space:                      35.8 MB, beat 35.10%
 */
