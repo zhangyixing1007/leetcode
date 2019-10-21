5af2342f7dfc2ff0ca43724b4029efd92883194a
zhangyixing1007 <41513796+zhangyixing1007@users.noreply.github.com>
2019-10-21 14:38:16 +0800

    Create Solution.java
    /*
    LeetCode Problem No.322:    https://leetcode.com/problems/coin-change
    Author:                     zhangyixing1007
    Idea:                       dynamic programming
    Time:                       14 ms, beat 90.05%
    Space:                      35.8 MB, beat 95.25%
    */

---
+++ 322_coinChange/Solution.java
@@ -0,0 +1,24 @@
+/*
+LeetCode Problem No.322:    https://leetcode.com/problems/coin-change
+Author:                     zhangyixing1007
+Idea:                       dynamic programming
+Time:                       14 ms, beat 90.05%
+Space:                      35.8 MB, beat 95.25%
+*/
+
+class Solution {
+    public int coinChange(int[] coins, int amount) {
+        if (amount <= 0) return 0;
+        int[] dp = new int[amount+1];
+        Arrays.fill(dp,amount+1);
+        for (int i = 1; i < dp.length; i++){
+            for (int j = 0; j < coins.length; j++){
+                int coin = coins[j];
+                if (i==coin) dp[i] = 1;
+                if (i>coin)
+                    dp[i] = Math.min(dp[i],dp[i - coin]+1);     
+            }
+        }
+        return dp[amount]==amount+1? -1:dp[amount];
+    }
+}
