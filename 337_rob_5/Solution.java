0ec4fa1d21268a4a5efb2dd9f3b00580575f6cd9
zhangyixing1007 <41513796+zhangyixing1007@users.noreply.github.com>
2019-10-22 13:30:25 +0800

    Create Solution.java
    /*
    LeetCode Problem No.337:    https://leetcode.com/problems/house-robber-iii
    Author:                     zhangyixing1007
    Idea:                       recursion, based on Version 4, simply calculation for ans[0];
    Time:                       1 ms, beat 100%
    Space:                      36.4 MB, beat 96.71%
    */

---
+++ 337_rob_5/Solution.java
@@ -0,0 +1,37 @@
+/*
+LeetCode Problem No.337:    https://leetcode.com/problems/house-robber-iii
+Author:                     zhangyixing1007
+Idea:                       recursion, based on Version 4, simply calculation for ans[0];
+Time:                       1 ms, beat 100%
+Space:                      36.4 MB, beat 96.71%
+*/
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+ 
+class Solution {
+    public int rob(TreeNode root) {
+        int[] ans = robOrNot(root);
+        return Math.max(ans[0],ans[1]);    
+    }
+    
+    private int[] robOrNot(TreeNode root){
+        int[] ans = new int[2];
+        if (root == null) return ans;
+        
+        int[] left = robOrNot(root.left);
+        int[] right = robOrNot(root.right);
+        
+        ans[0] = Math.max(left[0],left[1]) + Math.max(right[0],right[1]);
+        ans[1] = left[0]+right[0]+root.val;
+        
+        return ans;
+    }
+}
