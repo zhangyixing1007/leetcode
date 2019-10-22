713d7a3f86027f3c505beb3c5561bbd0130213f1
zhangyixing1007 <41513796+zhangyixing1007@users.noreply.github.com>
2019-10-22 12:34:35 +0800

    Create Solution.java
    /*
    LeetCode Problem No.338:    https://leetcode.com/problems/counting-bits
    Author:                     zhangyixing1007
    Idea:                       iteration, arithmetic shift to the right actually
    Time:                       2 ms, beat 94.32%
    Space:                      38.8 MB, beat 86.33%
    */

---
+++ 338_countBits_3/Solution.java
@@ -0,0 +1,17 @@
+/*
+LeetCode Problem No.338:    https://leetcode.com/problems/counting-bits
+Author:                     zhangyixing1007
+Idea:                       iteration, arithmetic shift to the right actually
+Time:                       2 ms, beat 94.32%
+Space:                      38.8 MB, beat 86.33%
+*/
+
+class Solution {
+    public int[] countBits(int num) {
+        int[] ans = new int[num+1];    
+        for (int i = 0; i < num+1; i++)  {
+            ans[i] = ans[i/2] + i%2;
+        }
+        return ans;
+    }
+}
