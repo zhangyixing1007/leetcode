ad66cee1e74cab9f498e44dd0334dc038de5e963
zhangyixing1007 <41513796+zhangyixing1007@users.noreply.github.com>
2019-10-29 20:40:25 +0800

    Update Solution.java
    /*
    LeetCode Problem No.208:    https://leetcode.com/problems/implement-trie-prefix-tree/
    Author:                     zhangyixing1007
    Idea:                       TrieNode, TrieNode[] as pointer
    Time:                       100 ms, beat 94.11%
    Space:                      50.2 MB, beat 99.16%
    */

--- 208_Trie_2/Solution.java
+++ 208_Trie_2/Solution.java
@@ -6,68 +6,56 @@ Time:                       100 ms, beat 94.11%
 Space:                      50.2 MB, beat 99.16%
 */
 
-class Trie 
-{
-    
-    class TrieNode
-    {
-        boolean end = false;
+class Trie {
+    class TrieNode{
         TrieNode[] next = new TrieNode[26];
+        boolean end = false;
+        TrieNode() {}
         
-        private TrieNode(){}
-        
-        private TrieNode add(char c)
-        {
+        TrieNode add(char c){
             int k = c-'a';
-            if(next[k]==null) next[k] = new TrieNode();
+            if (next[k] == null) 
+                next[k] = new TrieNode();
             return next[k];
         }
         
-        private TrieNode get(char c)
-        {return next[c-'a'];  }                 
-    }
-
-    /** Initialize your data structure here. */
-    public Trie() 
-    {
-        root = new TrieNode();
+        TrieNode get(char c){
+            return next[c-'a'];
+        }
     }
     
-    private TrieNode root;
+    TrieNode root = new TrieNode();
+    
+    /** Initialize your data structure here. */
+    public Trie() {}
     
     /** Inserts a word into the trie. */
-    public void insert(String word) 
-    {
-        TrieNode head = root;
-        for (char c:word.toCharArray())
-        {
-            head = head.add(c);
+    public void insert(String word) {
+        TrieNode node = root;
+        for (char c:word.toCharArray()){
+            node = node.add(c);
         }
-        head.end = true;
+        node.end = true;
     }
     
     /** Returns if the word is in the trie. */
-    public boolean search(String word) 
-    {
-        TrieNode head = root;
-        for (char c : word.toCharArray())
-        {
-            head = head.get(c);
-            if (head == null) return false;
+    public boolean search(String word) {
+        TrieNode node = root;
+        for (char c:word.toCharArray()){
+            node = node.get(c);
+            if (node == null) return false;
         }
-        return head.end;        
+        return node.end;
     }
     
     /** Returns if there is any word in the trie that starts with the given prefix. */
-    public boolean startsWith(String prefix) 
-    {
-        TrieNode head = root;
-        for (char c:prefix.toCharArray())
-        {
-            head = head.get(c);
-            if (head == null) return false;
+    public boolean startsWith(String prefix) {
+        TrieNode node = root;
+        for (char c:prefix.toCharArray()){
+            node = node.get(c);
+            if (node == null) return false;
         }
-        return true;         
+        return true;        
     }
 }
 
