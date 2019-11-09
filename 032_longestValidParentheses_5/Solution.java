b5f556b05667e5523b11744230557384c3a8429b
zhangyixing1007 <41513796+zhangyixing1007@users.noreply.github.com>
2019-11-09 15:28:35 +0800

    Create Solution.java
    /*
    LeetCode Problem No.032:    https://leetcode.com/problems/longest-valid-parentheses/
    Author:                     zhangyixing1007
    Idea:                       scan from left and right respectivly
    Time:                       2 ms, beat 99.83%
    Space:                      36.8 MB, beat 87.26%
    */

---
+++ 032_longestValidParentheses_5/Solution.java
@@ -0,0 +1,27 @@
+/*
+LeetCode Problem No.032:    https://leetcode.com/problems/longest-valid-parentheses/
+Author:                     zhangyixing1007
+Idea:                       scan from left and right respectivly
+Time:                       2 ms, beat 99.83%
+Space:                      36.8 MB, beat 87.26%
+*/
+
+class Solution {
+    public int longestValidParentheses(String s) {
+        if (s.length() < 2) 
+            return 0;
+        int ans = 0;
+        int[] dp = new int[s.length()+1];
+        for (int i = 2; i < dp.length; i++){
+            if (s.charAt(i-1) == ')'){
+                if (s.charAt(i-2) == '('){
+                    dp[i] = dp[i-2] + 2;
+                } else if (i-1-dp[i-1] > 0 && s.charAt(i-2-dp[i-1]) == '('){
+                    dp[i] = dp[i-1] + dp[i-2-dp[i-1]]+2;
+                }
+                ans = Math.max(ans, dp[i]);
+            }
+        }
+        return ans;
+    }
+}
