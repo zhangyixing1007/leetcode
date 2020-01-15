f87db13988a6718266f99ddc37002fb15a891684
zhangyixing1007 <41513796+zhangyixing1007@users.noreply.github.com>
2020-01-15 11:56:34 +0800

    start from the leftest and downest corner
    /*
    LeetCode Problem No.240:    https://leetcode.com/problems/search-a-2d-matrix-ii/
    Author:                     zhangyixing1007
    Idea:                       start from the leftest and downest corner, if larger than target, move to right, otherwise, up
    Time:                       7 s, beat 98.74%
    Space:                      42.7 MB, beat 97.30%
    */

--- 240_searchMatrix_3/Solution.java
+++ 240_searchMatrix_3/Solution.java
@@ -6,20 +6,16 @@ Time:                       7 s, beat 98.74%
 Space:                      42.7 MB, beat 97.30%
 */
 
-class Solution
-{
-    public boolean searchMatrix(int[][] matrix, int target)
-    {
-        if (matrix==null||matrix.length==0|| matrix[0].length==0) return false;
-        int row = matrix.length-1;
-        int col = 0;
-        
-        while (true)
-        {
-            if (row<0||col>matrix[0].length-1) return false;
-            if (matrix[row][col] > target) {row--;}
-            else if (matrix[row][col] < target) {col++;}
-            else {return true;}
-        }
+class Solution {
+    public boolean searchMatrix(int[][] matrix, int target) {
+        if (matrix == null || matrix.length == 0 || matrix[0].length == 0) return false;
+        if (matrix[0][0] > target || matrix[matrix.length-1][matrix[0].length-1] < target) return false;
+        int row = matrix.length-1, col = 0;
+        while (true){
+            if (row < 0 || col >= matrix[0].length) return false;
+            if (matrix[row][col] > target) row--;
+            else if (matrix[row][col] < target) col++;
+            else return true;
+        }    
     }
 }
