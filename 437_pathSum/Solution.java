85c4d77490937eef8cd3b7a56f591f60ab13a6db
zhangyixing1007 <41513796+zhangyixing1007@users.noreply.github.com>
2020-01-16 17:55:26 +0800

    tree = root+left-subtree+right-subtree
    /*
    LeetCode Problem No.437:    https://leetcode.com/problems/path-sum-iii/
    Author:                     zhangyixing1007
    Idea:                       tree = root+left-subtree+right-subtree
    Time:                       19 ms, beat 58.67%
    Space:                      39.8MB, beat 60.16%
    */

--- 437_pathSum/Solution.java
+++ 437_pathSum/Solution.java
@@ -1,7 +1,7 @@
 /*
 LeetCode Problem No.437:    https://leetcode.com/problems/path-sum-iii/
 Author:                     zhangyixing1007
-Idea:                       interesting two-layer recursion
+Idea:                       tree = root+left-subtree+right-subtree
 Time:                       19 ms, beat 58.67%
 Space:                      39.8MB, beat 60.16%
 */
@@ -15,17 +15,17 @@ Space:                      39.8MB, beat 60.16%
 *     TreeNode(int x) { val = x; } 
 * } 
 */ 
-class Solution { 
-    public int pathSum(TreeNode root, int sum) { 
-        if (root==null) return 0; 
-        return p(root,sum)+pathSum(root.left,sum) + pathSum(root.right,sum); 
-    } 
-     
-    private int p(TreeNode root, int sum) 
-    { 
-        if (root==null) return 0; 
-        if (sum==root.val) 
-            return p(root.left,sum-root.val)+p(root.right,sum-root.val)+1; 
-        return p(root.left,sum-root.val)+p(root.right,sum-root.val); 
-    } 
-} 
+class Solution {
+    public int pathSum(TreeNode root, int sum) {
+        if (root==null) return 0;
+        return p(root,sum)+pathSum(root.left,sum)+pathSum(root.right,sum);
+    }
+    int p(TreeNode root, int sum){
+        if (root==null) return 0;
+        int ans = 0;
+        if (sum==root.val) ans++;
+        ans+=p(root.left, sum-root.val);
+        ans+=p(root.right, sum-root.val); 
+        return ans;
+    }
+}
