ffd1fc9f5471b6ee99291e2c0122606531e6f188
zhangyixing1007 <41513796+zhangyixing1007@users.noreply.github.com>
2020-01-17 22:09:52 +0800

    prev = null, two pointers, move
    LeetCode Problem No.206:    https://leetcode.com/problems/reverse-linked-list/
    Author:                     zhangyixing1007
    Idea:                       prev = null, two pointers, move
    Time:                       0 ms, beat 100%
    Space:                      36.7MB, beat 54.36%
    */
    =================================
    original version:
    
    class Solution { 
        public ListNode reverseList(ListNode head) { 
            if (head == null) return null; 
             
            ListNode prev = null, curr = head, next = head.next;         
            while(next!=null) 
            { 
                curr.next = prev; 
                prev = curr; curr = next; next = next.next; 
            } 
            curr.next = prev; 
            return curr; 
        } 
    }

--- 206_reverseList/Solution.java
+++ 206_reverseList/Solution.java
@@ -6,17 +6,16 @@ Time:                       0 ms, beat 100%
 Space:                      36.7MB, beat 54.36%
 */
 
-class Solution { 
-    public ListNode reverseList(ListNode head) { 
-        if (head == null) return null; 
-         
-        ListNode prev = null, curr = head, next = head.next;         
-        while(next!=null) 
-        { 
-            curr.next = prev; 
-            prev = curr; curr = next; next = next.next; 
-        } 
-        curr.next = prev; 
-        return curr; 
-    } 
-} 
+class Solution {
+    public ListNode reverseList(ListNode head) {
+        ListNode prev=null;
+        while(head!=null){
+            ListNode tmp=head.next;
+            head.next=prev;
+            prev=head;
+            head=tmp;
+        }
+        return prev;
+    }
+}
+
