d247f326d37ef3877929636c67e01d40cca90e84
zhangyixing1007 <41513796+zhangyixing1007@users.noreply.github.com>
2020-01-18 00:28:55 +0800

    exchange nums[i] with the first zero
    /*
    LeetCode Problem No.283:    https://leetcode.com/problems/move-zeroes/
    Author:                     zhangyixing1007
    Idea:                       exchange nums[i] with the first zero 
    Time:                       0 ms, beat 100%
    Space:                      37.3MB, beat 95.82%
    */

--- 283_moveZeroes/Solution.java
+++ 283_moveZeroes/Solution.java
@@ -1,17 +1,15 @@
 /*
 LeetCode Problem No.283:    https://leetcode.com/problems/move-zeroes/
 Author:                     zhangyixing1007
-Idea:                       exchange nums[i] with the first zero appearred
+Idea:                       exchange nums[i] with the first zero 
 Time:                       0 ms, beat 100%
 Space:                      37.3MB, beat 95.82%
 */
 
 class Solution { 
     public void moveZeroes(int[] nums) { 
-        for (int i = 0, j = 0; i < nums.length; i++) 
-        { 
-            if(nums[i]!=0) 
-            { 
+        for (int i = 0, j = 0; i < nums.length; i++) { 
+            if(nums[i]!=0) { 
                 if(i!=j) 
                 {int tmp = nums[i];nums[i] = nums[j];nums[j] = tmp;}                
                 j++; 
