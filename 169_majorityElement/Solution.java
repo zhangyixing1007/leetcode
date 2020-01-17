36d0872ac4492b4f55fd13020e54ba30c1b002f4
zhangyixing1007 <41513796+zhangyixing1007@users.noreply.github.com>
2020-01-17 15:58:18 +0800

    majority and minority commit suicide in pairs
    /*
    LeetCode Problem No.169:    https://leetcode.com/problems/majority-element/
    Author:                     zhangyixing1007
    Idea:                       majority and minority commit suicide in pairs
    Time:                       4 ms, beat 70.12%
    Space:                      41.9MB, beat 83.65%
    */

--- 169_majorityElement/Solution.java
+++ 169_majorityElement/Solution.java
@@ -1,7 +1,7 @@
 /*
 LeetCode Problem No.169:    https://leetcode.com/problems/majority-element/
 Author:                     zhangyixing1007
-Idea:                       commit suicide in pairs
+Idea:                       majority and minority commit suicide in pairs
 Time:                       4 ms, beat 70.12%
 Space:                      41.9MB, beat 83.65%
 */
@@ -9,9 +9,8 @@ Space:                      41.9MB, beat 83.65%
 class Solution { 
     public int majorityElement(int[] nums) { 
         int count = 0; 
-        Integer candidate = null; 
-        for (int num : nums) 
-        { 
+        int candidate = nums[0]; 
+        for (int num : nums) { 
             if (count == 0) candidate = num; 
             count += (num == candidate) ? 1 : -1; 
         } 
