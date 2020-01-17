29e13adfb7929703f63fe6a00ca60f71d8282fad
zhangyixing1007 <41513796+zhangyixing1007@users.noreply.github.com>
2020-01-17 11:55:13 +0800

    put them in right replace, and count
    /*
    LeetCode Problem No.448:    https://leetcode.com/problems/find-all-numbers-disappeared-in-an-array/
    Author:                     zhangyixing1007
    Idea:                       put nums[i] to where it should be until that place has been occupied correctly
    Time:                       7 ms, beat 98.42%
    Space:                      47.3MB, beat 95.66%
    */

--- 448_findDisappearedNumbers/Solution.java
+++ 448_findDisappearedNumbers/Solution.java
@@ -1,30 +1,23 @@
 /*
 LeetCode Problem No.448:    https://leetcode.com/problems/find-all-numbers-disappeared-in-an-array/
 Author:                     zhangyixing1007
-Idea:                       put nums[i] to where it should be until that place has been occupied correctly
+Idea:                       put nums[i] to where it should be and then count
 Time:                       7 ms, beat 98.42%
 Space:                      47.3MB, beat 95.66%
 */
 
-class Solution { 
-    public List<Integer> findDisappearedNumbers(int[] nums) { 
-        List<Integer> list = new ArrayList<>(); 
-         
-        for (int i = 0; i < nums.length; i++) 
-        { 
-            while (nums[i]!=nums[nums[i]-1]) 
-            { 
-                int t = nums[i]; nums[i] = nums[nums[i]-1];nums[t-1] = t; 
-            } 
-        } 
-         
-        for (int i = 0; i < nums.length; i++) 
-        { 
-            if (nums[i]!=i+1) 
-            { 
-               list.add(i+1); 
-            } 
-        }         
-        return list; 
-    } 
-} 
+class Solution {
+    public List<Integer> findDisappearedNumbers(int[] nums) {
+        for (int i=0; i<nums.length; i++){
+            int change=nums[i];
+            while (change!=nums[change-1]) {
+                int tmp=nums[change-1];nums[change-1]=change;change=tmp;
+            }
+        }
+        List<Integer> list = new ArrayList<>();
+        for (int i=0; i<nums.length; i++){
+            if (nums[i]!=i+1) list.add(i+1);
+        }
+        return list;
+    }
+}
