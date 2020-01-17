2b001b3f065656279383bfefc5021a97547a8148
zhangyixing1007 <41513796+zhangyixing1007@users.noreply.github.com>
2020-01-17 17:59:30 +0800

    a XOR a = 0, a XOR 0 = a
    /*
    LeetCode Problem No.136:    https://leetcode.com/problems/single-number/
    Author:                     zhangyixing1007
    Idea:                       property of XOR, a XOR a = 0, a XOR 0 = a
    Time:                       1 ms, beat 99.87%
    Space:                      38 MB, beat 97.47%
    */

--- 136_singleNumber/Solution.java
+++ 136_singleNumber/Solution.java
@@ -8,12 +8,9 @@ Space:                      38 MB, beat 97.47%
 
 class Solution {
     public int singleNumber(int[] nums) {
-        if (nums.length == 0) return 0;
-        if (nums.length == 1) return nums[0];
-        
-        for (int i=1; i<nums.length; i++)
-        {nums[0] = nums[0] ^ nums[i];}
-        
+        for (int i=1; i<nums.length; i++){
+            nums[0]^=nums[i];
+        }
         return nums[0];
     }
 }
