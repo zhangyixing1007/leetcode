3bada8f831177f9ea533f36600293f1fc4cc9bd3
zhangyixing1007 <41513796+zhangyixing1007@users.noreply.github.com>
2020-01-17 17:26:17 +0800

    fast/slow pointers
    /*
    LeetCode Problem No.141:    https://leetcode.com/problems/linked-list-cycle/
    Author:                     zhangyixing1007
    Idea:                       two pointers, fast and slow
    Time:                       0 ms, beat 100%
    Space:                      36.2MB, beat 99.61%
    */

--- 141_hasCycle/Solution.java
+++ 141_hasCycle/Solution.java
@@ -3,33 +3,30 @@ LeetCode Problem No.141:    https://leetcode.com/problems/linked-list-cycle/
 Author:                     zhangyixing1007
 Idea:                       two pointers, fast and slow
 Time:                       0 ms, beat 100%
-Space:                      37.5MB, beat 96.69%
+Space:                      36.2MB, beat 99.61%
 */
-
 
 /**
-* Definition for singly-linked list.
-* class ListNode {
-*     int val;
-*     ListNode next;
-*     ListNode(int x) {
-*         val = x;
-*         next = null;
-*     }
-* }
-*/
+ * Definition for singly-linked list.
+ * class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) {
+ *         val = x;
+ *         next = null;
+ *     }
+ * }
+ */
 
 public class Solution {
     public boolean hasCycle(ListNode head) {
-        ListNode fast = head;
-        ListNode slow = head;
-        while (head!=null)
-        {
-            if (fast == null||fast.next==null) return false;
-            else if (fast.next != null) fast = fast.next.next;
-            slow = slow.next;
-            if (fast == slow) return true;
+        if(head==null) return false;
+        ListNode slow=head, fast=head.next;
+        while (slow!=fast){
+            if(fast==null||fast.next==null) return false;
+            slow=slow.next;
+            fast=fast.next.next;
         }
-        return false;
+        return true;
     }
 }
