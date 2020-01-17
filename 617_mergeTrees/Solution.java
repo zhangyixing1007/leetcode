5916a780375444094c48133aa355f981fbb2a71d
zhangyixing1007 <41513796+zhangyixing1007@users.noreply.github.com>
2020-01-17 12:11:59 +0800

    recursion, tree=root+left-subtree+right-subtree
    /*
    LeetCode Problem No.617:    https://leetcode.com/problems/merge-two-binary-trees/
    Author:                     zhangyixing1007
    Idea:                       recursion, tree=root+left-subtree+right-subtree
    Time:                       1 ms, beat 99.91%
    Space:                      40.3 MB, beat 95.37%
    */

--- 617_mergeTrees/Solution.java
+++ 617_mergeTrees/Solution.java
@@ -1,7 +1,7 @@
 /*
 LeetCode Problem No.617:    https://leetcode.com/problems/merge-two-binary-trees/
 Author:                     zhangyixing1007
-Idea:                       loop once save values in HashMap, and check its complement
+Idea:                       recursion, tree=root+left-subtree+right-subtree
 Time:                       1 ms, beat 99.91%
 Space:                      40.3 MB, beat 95.37%
 */
@@ -15,14 +15,14 @@ Space:                      40.3 MB, beat 95.37%
  *     TreeNode(int x) { val = x; } 
  * } 
  */ 
-class Solution { 
-    public TreeNode mergeTrees(TreeNode t1, TreeNode t2) { 
-        if (t1==null&&t2==null) return null; 
-        if (t1==null&&t2!=null) return t2; 
-        if (t1!=null&&t2==null) return t1; 
-        t1.val += t2.val; 
-        t1.left = mergeTrees(t1.left,t2.left); 
-        t1.right = mergeTrees(t1.right,t2.right); 
-        return t1; 
-    } 
-} 
+class Solution {
+    public TreeNode mergeTrees(TreeNode t1, TreeNode t2) {
+        if(t1==null&&t2==null) return null;
+        if(t1==null) return t2;
+        if(t2==null) return t1;
+        t1.val+=t2.val; 
+        t1.left=mergeTrees(t1.left,t2.left);
+        t1.right=mergeTrees(t1.right, t2.right);
+        return t1;
+    }
+}
