326d073150e8d75b028269384670ee4bbc5577df
zhangyixing1007 <41513796+zhangyixing1007@users.noreply.github.com>
2020-01-17 15:50:43 +0800

    daily recursion
    /*
    LeetCode Problem No.101:    https://leetcode.com/problems/symmetric-tree/
    Author:                     zhangyixing1007
    Idea:                       recursion
    Time:                       1 ms, beat 99.31%
    Space:                      37.5MB, beat 68.40%
    */

--- 101_isSymmetric/Solution.java
+++ 101_isSymmetric/Solution.java
@@ -15,6 +15,7 @@ Space:                      37.5MB, beat 68.40%
  *     TreeNode(int x) { val = x; }
  * }
  */
+
 class Solution {
     public boolean isSymmetric(TreeNode root) {
         if (root == null) return true;
@@ -23,7 +24,8 @@ class Solution {
     
     private boolean checkChildren (TreeNode left, TreeNode right)
     {
-        if ((left!=null && right!=null)==false) return (left==null && right == null);            
+        if (left==null||right==null) 
+            return (left==null && right == null);            
         if (left.val != right.val) return false;
         return (checkChildren(left.left,right.right) && checkChildren(left.right,right.left));
     }
