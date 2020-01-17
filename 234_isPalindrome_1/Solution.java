f120255d12b3485fe371a5fdaaa8776d2027073b
zhangyixing1007 <41513796+zhangyixing1007@users.noreply.github.com>
2020-01-17 16:26:27 +0800

    fast/slow pointers, reverse 1/2 (count the length)
    /*
    LeetCode Problem No.234:    https://leetcode.com/problems/palindrome-linked-list/
    Author:                     zhangyixing1007
    Idea:                       fast/slow pointers, reverse 1/2 (count the length)
    Time:                       1 ms, beat 99.89%
    Space:                      40.4MB, beat 97.76%
    */

--- 234_isPalindrome_1/Solution.java
+++ 234_isPalindrome_1/Solution.java
@@ -1,7 +1,7 @@
 /*
 LeetCode Problem No.234:    https://leetcode.com/problems/palindrome-linked-list/
 Author:                     zhangyixing1007
-Idea:                       fast/slow pointers, reverse one half of it and compare, pay attention to odd/even case
+Idea:                       fast/slow pointers, reverse 1/2 (count the length)
 Time:                       1 ms, beat 99.89%
 Space:                      40.4MB, beat 97.76%
 */
@@ -15,23 +15,19 @@ class Solution {
         int len = 1; 
         ListNode fast = head; 
         ListNode slow = head; 
-        while (fast!=null && fast.next!=null && fast.next.next!=null) 
-        { 
+        while (fast!=null && fast.next!=null && fast.next.next!=null) { 
             fast = fast.next.next; 
             slow = slow.next; len ++;          
         } 
-        if (fast.next!=null) 
-        {             
+        if (fast.next!=null) {             
             slow = slow.next; 
         } 
         int[] compare = new int[len]; 
-        for (int i = 0; i < len; i++) 
-        { 
+        for (int i = 0; i < len; i++) { 
             compare[len-i-1] = slow.val; 
             slow = slow.next; 
         } 
-        for (int i = 0; i < len; i++) 
-        { 
+        for (int i = 0; i < len; i++) { 
             if(head.val == compare[i]) 
             {head=head.next;} 
             else 
