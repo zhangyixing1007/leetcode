5df320f4a35e0890a29149c3c1f5c6377686b9d6
zhangyixing1007 <41513796+zhangyixing1007@users.noreply.github.com>
2020-01-17 18:09:03 +0800

    take difference with so far min, find the max
    /*
    LeetCode Problem No.121:    https://leetcode.com/problems/best-time-to-buy-and-sell-stock/
    Author:                     zhangyixing1007
    Idea:                       take difference with so far min, find the max
    Time:                       1 ms, beat 99.96%
    Space:                      36.7MB, beat 95.21%
    */
    ===========================
    original version: 
    
    class Solution {
        public int maxProfit(int[] prices) {
            if (prices.length <2) return 0;
            
            int minSoFar = prices[0];
            int maxPriceDiff = 0;
            
            for (int i = 1; i<prices.length; i++)
            {
                if (minSoFar>prices[i]) minSoFar = prices[i];
                if (prices[i]-minSoFar>maxPriceDiff) maxPriceDiff = prices[i]-minSoFar;
            }
            if (maxPriceDiff>0) return maxPriceDiff;
            else return 0;
        }
    }

--- 121_maxProfit/Solution.java
+++ 121_maxProfit/Solution.java
@@ -1,24 +1,18 @@
 /*
 LeetCode Problem No.121:    https://leetcode.com/problems/best-time-to-buy-and-sell-stock/
 Author:                     zhangyixing1007
-Idea:                       similar as Problem 53, maxEndingHere = rices[i]-minSoFar, maxSoFar = maxPriceDiff
+Idea:                       take difference with so far min, find the max
 Time:                       1 ms, beat 99.96%
-Space:                      36.7MB, beat 95.21%, if "return maxPriceDiff", space cost will be higher
+Space:                      36.7MB, beat 95.21%
 */
 
 class Solution {
     public int maxProfit(int[] prices) {
-        if (prices.length <2) return 0;
-        
-        int minSoFar = prices[0];
-        int maxPriceDiff = 0;
-        
-        for (int i = 1; i<prices.length; i++)
-        {
-            if (minSoFar>prices[i]) minSoFar = prices[i];
-            if (prices[i]-minSoFar>maxPriceDiff) maxPriceDiff = prices[i]-minSoFar;
+        int min=Integer.MAX_VALUE, max=0;
+        for(int price:prices){
+            min=Math.min(min,price);
+            if(price-min>max) max=price-min; 
         }
-        if (maxPriceDiff>0) return maxPriceDiff;
-        else return 0;
+        return max;
     }
 }
