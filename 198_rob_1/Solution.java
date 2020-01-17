6e4a2cea1ead799048b5d7aba728ef35aafdfc2e
zhangyixing1007 <41513796+zhangyixing1007@users.noreply.github.com>
2020-01-17 17:45:58 +0800

    simple recursion, no saving
    /*
    LeetCode Problem No.198:    https://leetcode.com/problems/house-robber/
    Author:                     zhangyixing1007
    Idea:                       simple recursion, no saving
    Time:                       4 ms, beat 70.21%
    Space:                      41.9MB, beat 83.65%
    */

--- 198_rob_1/Solution.java
+++ 198_rob_1/Solution.java
@@ -1,7 +1,7 @@
 /*
 LeetCode Problem No.198:    https://leetcode.com/problems/house-robber/
 Author:                     zhangyixing1007
-Idea:                       recursion
+Idea:                       simple recursion, no saving
 Time:                       4 ms, beat 70.21%
 Space:                      41.9MB, beat 83.65%
 */
@@ -10,8 +10,7 @@ class Solution {
     public int rob(int[] nums) { 
         int first = 0; 
         int second = 0; 
-        for (int num:nums) 
-        { 
+        for (int num:nums) { 
             int tmp = second; 
             second = Math.max(first+num,second); 
             first = tmp; 
