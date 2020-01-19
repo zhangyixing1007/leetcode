58e0526bf860e992bae897819ffbf26af91f52b6
zhangyixing1007 <41513796+zhangyixing1007@users.noreply.github.com>
2020-01-19 16:43:57 +0800

    2-fold iteration, 2-D array as intermediate saving
    /*
    LeetCode Problem No.64:     https://leetcode.com/problems/minimum-path-sum/
    Author:                     zhangyixing1007
    Idea:                       2-fold iteration, 2-D array as intermediate saving
    Time:                       4 ms, beat 85.06%
    Space:                      39.8 MB, beat 85.43%
    */

--- 064_minPathSum_2/Solution.java
+++ 064_minPathSum_2/Solution.java
@@ -14,10 +14,8 @@ class Solution {
         
         int[][] sum = new int[m][n];
         sum[0][0]  = grid[0][0];
-        for (int i = 0; i < m; i++)
-        {
-            for (int j = 0; j < n; j++)
-            {
+        for (int i = 0; i < m; i++){
+            for (int j = 0; j < n; j++){
                 if (i == 0 && j == 0) continue;
                 if (i == 0) sum[i][j] = grid[i][j] + sum[i][j-1]; 
                 else if (j == 0) sum[i][j] = grid[i][j] + sum[i-1][j];
