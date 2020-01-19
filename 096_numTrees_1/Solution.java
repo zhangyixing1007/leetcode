1a9b88efde6deff80c56b6ae88749b480f226ebe
zhangyixing1007 <41513796+zhangyixing1007@users.noreply.github.com>
2020-01-19 20:05:57 +0800

    2-layer loop
    /*
    LeetCode Problem No.96:     https://leetcode.com/problems/unique-binary-search-trees/
    Author:                     zhangyixing1007
    Idea:                       2-layer loop
    Time:                       0 ms, beat 100%
    Space:                      32.8 MB, beat 10.46%
    */

--- 096_numTrees_1/Solution.java
+++ 096_numTrees_1/Solution.java
@@ -1,7 +1,7 @@
 /*
 LeetCode Problem No.96:     https://leetcode.com/problems/unique-binary-search-trees/
 Author:                     zhangyixing1007
-Idea:                       recursion is very good, to save time, use array as intermediate saving
+Idea:                       2-layer loop
 Time:                       0 ms, beat 100%
 Space:                      32.8 MB, beat 10.46%
 */
@@ -9,14 +9,11 @@ Space:                      32.8 MB, beat 10.46%
 class Solution {
     public int numTrees(int n) {
         if (n <= 1) return 1;
-        // else if (n == 1) return 1;
         else if (n == 2) return 2;
         int[] count = new int[n+1];
         count[0] = 1; count[1] = 1; count[2] = 2;
-        for (int i = 3; i < count.length; i++)
-        {
-            for (int j = 0; j < i; j++)
-            {
+        for (int i = 3; i < count.length; i++){
+            for (int j = 0; j < i; j++){
                 count[i] += count[j] * count[i-j-1];
             }
         }
