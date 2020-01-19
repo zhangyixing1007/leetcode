fbff9afca7eae5821ab3aadb0f6fa509d5a549e2
zhangyixing1007 <41513796+zhangyixing1007@users.noreply.github.com>
2020-01-19 15:09:52 +0800

    HashMap, sort, conversion: char[] <-> String
    /*
    LeetCode Problem No.49:     https://leetcode.com/problems/group-anagrams/
    Author:                     zhangyixing1007
    Idea:                       make good use of HashMap, sort, conversion between char[] and String
    Time:                       16 ms, beat 88.71%
    Space:                      40.4 MB, beat 99.55%
    */

--- 049_groupAnagrams_2/Solution.java
+++ 049_groupAnagrams_2/Solution.java
@@ -1,23 +1,23 @@
 /*
 LeetCode Problem No.49:     https://leetcode.com/problems/group-anagrams/
 Author:                     zhangyixing1007
-Idea:                       the simplist brutal force, very very slow
+Idea:                       make good use of HashMap, sort, conversion between char[] and String
 Time:                       16 ms, beat 88.71%
 Space:                      40.4 MB, beat 99.55%
 */
 
 class Solution {
     public List<List<String>> groupAnagrams(String[] strs) {
-        // List<List<String>> list = new ArrayList<>();
-        Map<String, List<String>> map = new HashMap<>();
-        for (String str : strs)
-        {
-            char[] c = str.toCharArray();
+        HashMap<String, List<String>> map=new HashMap<>();
+        for (String str:strs){
+            char[] c=str.toCharArray();
             Arrays.sort(c);
-            String value = String.valueOf(c);
-            if (map.containsKey(value)) map.get(value).add(str);
-            else {map.put(value,new ArrayList<String>(Arrays.asList(str)));}
+            String s=String.valueOf(c);
+            if(map.containsKey(s))
+                map.get(s).add(str);
+            else
+                map.put(s,new ArrayList(Arrays.asList(str)));
         }
-        return new ArrayList<List<String>>(map.values());
+        return new ArrayList(map.values());
     }
 }
