6fdf40105b9762a478d8068c99cc695343284366
zhangyixing1007 <41513796+zhangyixing1007@users.noreply.github.com>
2020-01-19 21:38:41 +0800

    classical recursion, simplified version
    /*
    LeetCode Problem No.79:     https://leetcode.com/problems/word-search/
    Author:                     zhangyixing1007
    Idea:                       classical recursion
    Time:                       7 ms, beat 90.91%
    Space:                      38.9 MB, beat 96.71%
    */

--- 079_exist/Solution.java
+++ 079_exist/Solution.java
@@ -8,71 +8,39 @@ Space:                      38.9 MB, beat 96.71%
 
 class Solution {
     public boolean exist(char[][] board, String word) {
-        int n = word.length();
-        if (n==0) return true;
-        
-        this.board = board;
-        int now = 0;
-        for (int i = 0; i < board.length; i++)
-        {
-            for (int j = 0; j < board[0].length; j++)
-            {
-                if (word.charAt(0) == board[i][j])
-                {
-                    char c = board[i][j];
-                    this.board[i][j]='#';
-                    if (find(i,j,word,now)==true) return true;
-                    this.board[i][j] = c;
+        if (word==null||word.length()==0) return false;
+        for (int i=0; i<board.length; i++){
+            for (int j=0; j<board[0].length; j++){
+                if (board[i][j]==word.charAt(0)){
+                    board[i][j]='#';
+                    if (find(board,word,i,j,1)) 
+                        return true;   
+                    board[i][j]=word.charAt(0);
                 }
             }
         }
         return false;
     }
     
-    private char[][] board;
-    
-    private boolean find(int i, int j, String word, int now)
-    {
-        if (now==word.length()-1) return true;
-        now ++;
-        boolean result = false;
-        if (i < board.length-1 && board[i+1][j]!= '#'
-            && board[i+1][j] == word.charAt(now))
-            {
-                char c = board[i+1][j];
-                this.board[i+1][j]='#';
-                if (find(i+1,j,word,now)==true) return true;
-                this.board[i+1][j] = c;
-            }            
-
-
-        
-        if (i > 0 && board[i-1][j]!='#'
-            && board[i-1][j] == word.charAt(now))
-            {
-                char c = board[i-1][j];
-                this.board[i-1][j]='#';
-                if (find(i-1,j,word,now)==true) return true;
-                this.board[i-1][j] = c;
-            }       
-        
-        if (j < board[0].length-1 && board[i][j+1]!='#'
-            && board[i][j+1] == word.charAt(now))       
-            {
-                char c = board[i][j+1];
-                this.board[i][j+1]='#';
-                if (find(i,j+1,word,now) == true) return true;
-                this.board[i][j+1] = c;
-            }   
-        
-        if (j > 0 && board[i][j-1]!='#'
-            && board[i][j-1] == word.charAt(now))
-            {
-                char c = board[i][j-1];
-                this.board[i][j-1]='#';
-                if (find(i,j-1,word,now) == true) return true;
-                this.board[i][j-1] = c;
-            }               
+    boolean find(char[][] board, String word, int x, int y, int curr){
+        if (curr==word.length())
+            return true;
+        for (int i=Math.max(x-1,0); i<Math.min(x+2,board.length); i++){
+            if (board[i][y]==word.charAt(curr)){
+                board[i][y]='#';
+                if (find(board,word,i,y,curr+1)) 
+                    return true;   
+                board[i][y]=word.charAt(curr);
+            }
+        } 
+        for (int j=Math.max(y-1,0); j<Math.min(y+2,board[0].length); j++){
+            if (board[x][j]==word.charAt(curr)){
+                board[x][j]='#';
+                if (find(board,word,x,j,curr+1)) 
+                    return true;   
+                board[x][j]=word.charAt(curr);
+            }
+        }  
         return false;
     }
 }
