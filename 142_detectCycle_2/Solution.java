9b5198c8ed8faab60cdff947ac4afd72341d6653
zhangyixing1007 <41513796+zhangyixing1007@users.noreply.github.com>
2020-01-20 00:36:25 +0800

    two pointers +  Floyd Algorithm
    /*
    LeetCode Problem No.142:    https://leetcode.com/problems/linked-list-cycle-ii/
    Author:                     zhangyixing1007
    Idea:                       two pointers to check loop, and then Floyd Algorithm 
    Time:                       0 ms, beat 100%
    Space:                      36.1 MB, beat 5.03%
    */

--- 142_detectCycle_2/Solution.java
+++ 142_detectCycle_2/Solution.java
@@ -18,22 +18,18 @@ Space:                      36.1 MB, beat 5.03%
 * }
 */
 
-public class Solution
-{
-    public ListNode detectCycle(ListNode head)
-    {
+public class Solution{
+    public ListNode detectCycle(ListNode head){
         ListNode fast = head;
         ListNode slow = head;
-        while(fast!=null&&fast.next!=null)
-        {
+        while(fast!=null&&fast.next!=null){
             fast = fast.next.next;
             slow = slow.next;
             if (fast == slow) break;
         }
         if (fast==null||fast.next==null) return null;
         slow = head;
-        while (fast!=slow)
-        {
+        while (fast!=slow){
             fast = fast.next;
             slow = slow.next;
         }
