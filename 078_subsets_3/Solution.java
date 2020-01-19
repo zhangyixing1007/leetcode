9a1b299916f5a76b74ab0c80940d676b281144ee
zhangyixing1007 <41513796+zhangyixing1007@users.noreply.github.com>
2020-01-19 17:24:58 +0800

    backtrack, or say simply recursion
    /*
    LeetCode Problem No.78:     https://leetcode.com/problems/subsets/
    Author:                     zhangyixing1007
    Idea:                       backtrack, or say simply recursion
    Time:                       1 ms, beat 99.83%
    Space:                      35.9 MB, beat 74.86%
    */

--- 078_subsets_3/Solution.java
+++ 078_subsets_3/Solution.java
@@ -6,10 +6,8 @@ Time:                       1 ms, beat 99.83%
 Space:                      35.9 MB, beat 74.86%
 */
 
-class Solution
-{
-    public List<List<Integer>> subsets(int[] nums)
-    {
+class Solution{
+    public List<List<Integer>> subsets(int[] nums){
         List<Integer> l = new ArrayList<>();
         list = new ArrayList<>();
         list.add(l);
@@ -19,10 +17,8 @@ class Solution
     
     List<List<Integer>> list;
     
-    private void backtrack(List<Integer> l, int now, int[] nums)
-    {
-        if (now<nums.length)
-        {
+    private void backtrack(List<Integer> l, int now, int[] nums){
+        if (now<nums.length){
             List<Integer> t = new ArrayList<>(l);
             t.add(nums[now]);
             list.add(t);
