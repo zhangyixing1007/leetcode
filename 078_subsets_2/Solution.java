be0d5ee26d1a1e5eba9d5891d245f80eb611ffad
zhangyixing1007 <41513796+zhangyixing1007@users.noreply.github.com>
2020-01-19 17:23:00 +0800

    List to keep record
    /*
    LeetCode Problem No.78:     https://leetcode.com/problems/subsets/
    Author:                     zhangyixing1007
    Idea:                       List to keep record
    Time:                       1 ms, beat 99.83%
    Space:                      36.7 MB, beat 46.65%
    */

--- 078_subsets_2/Solution.java
+++ 078_subsets_2/Solution.java
@@ -6,18 +6,14 @@ Time:                       1 ms, beat 99.83%
 Space:                      36.7 MB, beat 46.65%
 */
 
-class Solution
-{
-    public List<List<Integer>> subsets(int[] nums)
-    {
+class Solution{
+    public List<List<Integer>> subsets(int[] nums){
         List<Integer> l = new ArrayList<>();
         List<List<Integer>> list = new ArrayList<>();
         list.add(l);
-        for (int b : nums)
-        {
+        for (int b : nums){
             int n = list.size();
-            for (int i = 0; i < n; i++)
-            {                
+            for (int i = 0; i < n; i++){                
                 List<Integer> s = list.get(i);                
                 List<Integer> t = new ArrayList<>(s);
                 t.add(b);
