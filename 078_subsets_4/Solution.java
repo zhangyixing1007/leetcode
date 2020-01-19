72904c6f167af70cdb2bde1631ce062b45a94a8f
zhangyixing1007 <41513796+zhangyixing1007@users.noreply.github.com>
2020-01-19 17:26:06 +0800

    bit operation, property of binary
    /*
    LeetCode Problem No.78:     https://leetcode.com/problems/subsets/
    Author:                     zhangyixing1007
    Idea:                       bit operation, property of binary
    Time:                       1 ms, beat 99.81%
    Space:                      36.9 MB, beat 45.19%
    */

--- 078_subsets_4/Solution.java
+++ 078_subsets_4/Solution.java
@@ -1,20 +1,17 @@
 /*
 LeetCode Problem No.78:     https://leetcode.com/problems/subsets/
 Author:                     zhangyixing1007
-Idea:                       bit operation
+Idea:                       bit operation, property of binary
 Time:                       1 ms, beat 99.81%
 Space:                      36.9 MB, beat 45.19%
 */
 
 class Solution {
-    public List<List<Integer>> subsets(int[] nums)
-    {
+    public List<List<Integer>> subsets(int[] nums){
         List<List<Integer>> list = new ArrayList<>();
-        for (int i = 0; i < 1 << nums.length; i++)
-        {
+        for (int i = 0; i < 1 << nums.length; i++){
             List<Integer> subset = new ArrayList<>();
-            for (int j = 0; j < nums.length; j++)
-            {
+            for (int j = 0; j < nums.length; j++){
                 if (((i>>j)&1)==1) 
                     subset.add(nums[j]);
             }
