86cd565c5272a4fd0207dd2581693bfa88dfc80d
zhangyixing1007 <41513796+zhangyixing1007@users.noreply.github.com>
2020-01-19 17:20:42 +0800

    queue to keep record
    /*
    LeetCode Problem No.78:     https://leetcode.com/problems/subsets/
    Author:                     zhangyixing1007
    Idea:                       queue to keep record
    Time:                       1 ms, beat 99.83%
    Space:                      36.1 MB, beat 64.41%
    */

--- 078_subsets_1/Solution.java
+++ 078_subsets_1/Solution.java
@@ -6,17 +6,13 @@ Time:                       1 ms, beat 99.83%
 Space:                      36.1 MB, beat 64.41%
 */
 
-class Solution
-{
-    public List<List<Integer>> subsets(int[] nums)
-    {        
+class Solution{
+    public List<List<Integer>> subsets(int[] nums){        
         Queue<List<Integer>> q = new LinkedList<>();
         List<Integer> l = new ArrayList<>();
         q.add(l);
-        for (int b : nums)
-        {
-            while (true)
-            {                
+        for (int b : nums){
+            while (true){                
                 List<Integer> s = q.poll();
                 q.add(s);
                 List<Integer> t = new ArrayList<>(s);
@@ -24,8 +20,7 @@ class Solution
                 q.add(t);
                 if (q.peek().size()==0) break;
             }
-        }
-            
+        }           
         List<List<Integer>> list = new ArrayList<>(q);
         return list;
     }
