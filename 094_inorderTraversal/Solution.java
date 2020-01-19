8d89f8128f5d3fe1b76d8fed1fb1f2e109180eb2
zhangyixing1007 <41513796+zhangyixing1007@users.noreply.github.com>
2020-01-19 20:33:14 +0800

    stack to keep record
    /*
    LeetCode Problem No.94:     https://leetcode.com/problems/binary-tree-inorder-traversal/
    Author:                     zhangyixing1007
    Idea:                       stack to keep record
    Time:                       1 ms, beat 98.08%
    Space:                      34.7 MB, beat 39.65%
    */

--- 094_inorderTraversal/Solution.java
+++ 094_inorderTraversal/Solution.java
@@ -1,7 +1,7 @@
 /*
 LeetCode Problem No.94:     https://leetcode.com/problems/binary-tree-inorder-traversal/
 Author:                     zhangyixing1007
-Idea:                       queue to keep record
+Idea:                       stack to keep record
 Time:                       1 ms, beat 98.08%
 Space:                      34.7 MB, beat 39.65%
 */
@@ -15,23 +15,19 @@ Space:                      34.7 MB, beat 39.65%
  *     TreeNode(int x) { val = x; }
  * }
  */
+
 class Solution {
-    public List<Integer> inorderTraversal(TreeNode root) 
-    {
-        List<Integer> list = new ArrayList<>();
-        Stack<TreeNode> stack = new Stack<>();
-         
-        TreeNode curr = root;
-        while (curr != null || !stack.isEmpty())
-        {
-            while (curr!=null)
-            {
-                stack.push(curr);
-                curr = curr.left;
+    public List<Integer> inorderTraversal(TreeNode root) {
+        List<Integer> list=new ArrayList<>();
+        Stack<TreeNode> stack=new Stack<>();
+        TreeNode curr=root;
+        while(curr!=null||!stack.isEmpty()){
+            while(curr!=null){               
+                stack.push(curr);curr=curr.left;
             }
-            curr = stack.pop();
+            curr=stack.pop();
             list.add(curr.val);
-            curr = curr.right;
+            curr=curr.right;
         }
         return list;
     }
