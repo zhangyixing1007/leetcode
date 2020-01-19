739d843785db7c8772fbc828509cdab77c5ba55f
zhangyixing1007 <41513796+zhangyixing1007@users.noreply.github.com>
2020-01-19 15:23:34 +0800

    one loop from end to start
    /*
    LeetCode Problem No.55:     https://leetcode.com/problems/jump-game/
    Author:                     zhangyixing1007
    Idea:                       one loop from end to start, find the minimum requested reach
    Time:                       1 ms, beat 100%
    Space:                      40.7MB, beat 82.63%
    */

--- 055_canJump_1/Solution.java
+++ 055_canJump_1/Solution.java
@@ -1,7 +1,7 @@
 /*
 LeetCode Problem No.55:     https://leetcode.com/problems/jump-game/
 Author:                     zhangyixing1007
-Idea:                       only one loop, start from the end of the array
+Idea:                       one loop from end to start, find the minimum requested reach
 Time:                       1 ms, beat 100%
 Space:                      40.7MB, beat 82.63%
 */
@@ -9,8 +9,7 @@ Space:                      40.7MB, beat 82.63%
 class Solution {
     public boolean canJump(int[] nums) {
         int reachable = nums.length==0? 0:nums.length-1;
-        for (int i = nums.length-1; i >= 0; i--)
-        {
+        for (int i = nums.length-1; i >= 0; i--){
             if (nums[i]+i>=reachable) reachable = i;
         }
         return (reachable==0? true:false);
