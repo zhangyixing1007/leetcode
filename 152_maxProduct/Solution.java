550b70123aa2474ae5db9158f779b9a5736d9b38
zhangyixing1007 <41513796+zhangyixing1007@users.noreply.github.com>
2020-01-25 15:46:15 +0800

    similar to 53, mind negative numbers
    /*
    LeetCode Problem No.152:    https://leetcode.com/problems/maximum-product-subarray/
    Author:                     zhangyixing1007
    Idea:                       similar to 53, mind negative numbers
    Time:                       2 ms, beat 98.47%
    Space:                      37 MB, beat 42.42%
    */

--- 152_maxProduct/Solution.java
+++ 152_maxProduct/Solution.java
@@ -1,26 +1,24 @@
 /*
 LeetCode Problem No.152:    https://leetcode.com/problems/maximum-product-subarray/
 Author:                     zhangyixing1007
-Idea:                       similar to 53
+Idea:                       similar to 53, mind negative numbers
 Time:                       2 ms, beat 98.47%
 Space:                      37 MB, beat 42.42%
 */
 
-class Solution
-{
-    public int maxProduct(int[] nums)
-    {
-        int imax = 1;
-        int imin = 1;
-        int max = Integer.MIN_VALUE;
-        for (int i = 0; i < nums.length; i++)
-        {
-            if (nums[i]<0)
-            {int tmp = imax; imax = imin; imin = tmp;}
-            imax = Math.max(imax*nums[i],nums[i]);
-            imin = Math.min(imin*nums[i],nums[i]);
-            max = Math.max(max,imax);
+class Solution {
+    public int maxProduct(int[] nums) {
+        if(nums==null||nums.length==0) return 0;
+        if(nums.length==1) return nums[0];
+        int max=1;
+        int min=1;
+        int maxSoFar=Integer.MIN_VALUE;
+        for (int i=0; i<nums.length; i++){
+            if (nums[i]<0){int t=max;max=min;min=t;}
+            max=Math.max(max*nums[i],nums[i]);
+            min=Math.min(min*nums[i],nums[i]);
+            maxSoFar=Math.max(maxSoFar, max);
         }
-        return max;
+        return maxSoFar;
     }
 }
