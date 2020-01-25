1b425a316f5c9815361c7defae307f27328b7e5a
zhangyixing1007 <41513796+zhangyixing1007@users.noreply.github.com>
2020-01-25 14:37:53 +0800

    recursion, keep the record
    /*
    LeetCode Problem No.139:    https://leetcode.com/problems/word-break/
    Author:                     zhangyixing1007
    Idea:                       recursion, keep the record
    Time:                       9 ms, beat 74.65%
    Space:                      36.8 MB, beat 77.49%
    */

--- 139_wordBreak_2/Solution.java
+++ 139_wordBreak_2/Solution.java
@@ -1,28 +1,26 @@
 /*
 LeetCode Problem No.139:    https://leetcode.com/problems/word-break/
 Author:                     zhangyixing1007
-Idea:                       check from the end of the String, memo keep the record
+Idea:                       recursion, keep the record
 Time:                       9 ms, beat 74.65%
 Space:                      36.8 MB, beat 77.49%
 */
 
-class Solution
-{
-    public boolean wordBreak(String s, List<String> wordDict)
-    {
-        return helper(s, new HashSet(wordDict), 0, new Boolean[s.length()]);
-    }
-    
-    private boolean helper(String s, HashSet<String> wordDict, int start, Boolean[] memo)
-    {
-        if (start == s.length()) return true;
-        if (memo[start]!=null) return memo[start];
-        
-        for (int end = start+1; end <= s.length(); end++)
-        {
-            if(wordDict.contains(s.substring(start,end))&&helper(s,wordDict,end,memo))
-                return memo[start] = true;
+class Solution {
+    public boolean wordBreak(String s, List<String> wordDict) {
+        if(s==null||s.length()==0||wordDict==null||wordDict.size()==0) return false;
+        return helper(s, new HashSet(wordDict), 0, new Boolean[s.length()+1]);
+    }    
+    private boolean helper(String s, HashSet<String> set, int start, Boolean[] record){
+        if(start==s.length()) return true;
+        if (record[start]!=null) return record[start];
+        for (int i=start+1; i<=s.length(); i++){
+            if(set.contains(s.substring(start,i))&&helper(s,set,i,record)){
+                record[start]=true;
+                return true;
+            }
         }
-        return memo[start] = false;
+        record[start]=false;
+        return false;
     }
 }
