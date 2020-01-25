fefcc28285223491090cc5a67ba2e7c2f3fbd8b7
zhangyixing1007 <41513796+zhangyixing1007@users.noreply.github.com>
2020-01-25 14:47:01 +0800

    iteration, keep the record
    /*
    LeetCode Problem No.139:    https://leetcode.com/problems/word-break/
    Author:                     zhangyixing1007
    Idea:                       iteration, keep the record
    Time:                       8 ms, beat 79.40%
    Space:                      36.4 MB, beat 80.84%
    */

--- 139_wordBreak_3/Solution.java
+++ 139_wordBreak_3/Solution.java
@@ -1,26 +1,26 @@
 /*
 LeetCode Problem No.139:    https://leetcode.com/problems/word-break/
 Author:                     zhangyixing1007
-Idea:                       check from the start of the String, "visited" keep the record
+Idea:                       iteration, keep the record
 Time:                       8 ms, beat 79.40%
 Space:                      36.4 MB, beat 80.84%
 */
 
-class Solution
-{
-    public boolean wordBreak(String s, List<String> wordDict)
-    {
-        boolean[] visited = new boolean[s.length()+1];
-        Set<String> dic = new HashSet(wordDict);
-        visited[0] = true;
-        for (int i = 0; i <= s.length(); i++)
-        {
-            for (int j = 0; j < i; j++)
-            {
-                if (visited[j]==true&&dic.contains(s.substring(j,i)))
-                    {visited[i] = true; break;}
+class Solution {
+    public boolean wordBreak(String s, List<String> wordDict) {
+        if(s==null||s.length()==0||wordDict==null||wordDict.size()==0) 
+            return false;
+        boolean[] visited=new boolean[s.length()+1];
+        HashSet<String> set= new HashSet<>(wordDict);
+        visited[0]=true;
+        for (int i=0; i<s.length(); i++){
+            if (visited[i]){
+                for (int j=i+1; j<s.length()+1; j++){
+                    if(set.contains(s.substring(i,j)))
+                        visited[j]=true;
+                }            
             }
         }
-        return visited[s.length()];
+        return visited[visited.length-1];
     }
 }
