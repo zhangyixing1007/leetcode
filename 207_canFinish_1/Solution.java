0d3ba506153d3479a7ff1f28df6549b1987f7438
zhangyixing1007 <41513796+zhangyixing1007@users.noreply.github.com>
2020-01-29 23:54:45 +0800

    topological sorting
    /*
    LeetCode Problem No.207:    https://leetcode.com/problems/course-schedule/
    Author:                     zhangyixing1007
    Idea:                       topological sorting 
    Time:                       38 ms, beat 61.13%
    Space:                      44.5 MB, beat 70.61%
    */

--- 207_canFinish_1/Solution.java
+++ 207_canFinish_1/Solution.java
@@ -1,41 +1,33 @@
 /*
 LeetCode Problem No.207:    https://leetcode.com/problems/course-schedule/
 Author:                     zhangyixing1007
-Idea:                       topological sorting
+Idea:                       topological sorting 
 Time:                       38 ms, beat 61.13%
 Space:                      44.5 MB, beat 70.61%
 */
 
-class Solution
-{
-    public boolean canFinish(int numCourses, int[][] prerequisites)
-    {
+class Solution{
+    public boolean canFinish(int numCourses, int[][] prerequisites){
         int[] count = new int[numCourses];
-        for (int i = 0; i < prerequisites.length; i++)
-        {
+        for (int i = 0; i < prerequisites.length; i++){
             count[prerequisites[i][1]]++;
         }
-        while (numCourses>1)
-        {
+        while (numCourses>1){
             numCourses --;
             int delete=-1;
-            for (int i = 0; i < count.length; i++)
-            {
+            for (int i = 0; i < count.length; i++){
                 if (count[i] == 0) {delete = i; break;}
             }
             if (delete == -1) return false;
             count[delete] = -1;
-            for (int i = 0; i < prerequisites.length; i++)
-            {
-                if (prerequisites[i][0] == delete)
-                {
+            for (int i = 0; i < prerequisites.length; i++){
+                if (prerequisites[i][0] == delete){
                     count[prerequisites[i][1]]--;
                     prerequisites[i][0] = -1;
                     prerequisites[i][1] = -1;                     
                 }
             }                     
         }
-        return true;
-        
+        return true;        
     }
 }
