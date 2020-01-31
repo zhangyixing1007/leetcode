51579c0e09d06213cc437ec39fb83af0068c5f1d
zhangyixing1007 <41513796+zhangyixing1007@users.noreply.github.com>
2020-01-31 19:01:53 +0800

    recursion, int[]
    /*
    LeetCode Problem No.337:    https://leetcode.com/problems/house-robber-iii
    Author:                     zhangyixing1007
    Idea:                       recursion, based on Version 4, simply calculation for ans[0];
    Time:                       1 ms, beat 100%
    Space:                      36.4 MB, beat 96.71%
    */

--- 337_rob_5/Solution.java
+++ 337_rob_5/Solution.java
@@ -18,20 +18,16 @@ Space:                      36.4 MB, beat 96.71%
  
 class Solution {
     public int rob(TreeNode root) {
-        int[] ans = robOrNot(root);
-        return Math.max(ans[0],ans[1]);    
+        int[] ans=robOrNot(root);
+        return Math.max(ans[0], ans[1]);
     }
-    
-    private int[] robOrNot(TreeNode root){
-        int[] ans = new int[2];
-        if (root == null) return ans;
-        
-        int[] left = robOrNot(root.left);
-        int[] right = robOrNot(root.right);
-        
-        ans[0] = Math.max(left[0],left[1]) + Math.max(right[0],right[1]);
-        ans[1] = left[0]+right[0]+root.val;
-        
+    int[] robOrNot(TreeNode root){
+        int[] ans=new int[2];
+        if (root==null) return ans;
+        int[] left=robOrNot(root.left);
+        int[] right=robOrNot(root.right);           
+        ans[0]=Math.max(left[0],left[1])+Math.max(right[0],right[1]);
+        ans[1]=root.val+left[0]+right[0];
         return ans;
     }
 }
