851ce842a8267387d1861d22e10d0b64b217337a
zhangyixing1007 <41513796+zhangyixing1007@users.noreply.github.com>
2020-01-31 16:11:22 +0800

    brute force
    /*
    LeetCode Problem No.287:    https://leetcode.com/problems/find-the-duplicate-number/
    Author:                     zhangyixing1007
    Idea:                       brute force
    Time:                       62 ms, beat 9.46%
    Space:                      37.2 MB, beat 95.29%
    */

--- 287_findDuplicate_1/Solution.java
+++ 287_findDuplicate_1/Solution.java
@@ -6,15 +6,13 @@ Time:                       62 ms, beat 9.46%
 Space:                      37.2 MB, beat 95.29%
 */
 
-class Solution
-{
-    public int findDuplicate(int[] nums)
-    {
+class Solution{
+    public int findDuplicate(int[] nums){
         if (nums.length==0) return -1;
-        for (int i = 1; i < nums.length; i++)
-        {
-            for (int j = 0; j < i; j++)
-            {if (nums[i]==nums[j]) return nums[i];}
+        for (int i = 1; i < nums.length; i++){
+            for (int j = 0; j < i; j++){
+                if (nums[i]==nums[j]) return nums[i];
+            }
         }        
         return -1;
     }
