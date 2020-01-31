8ee7e6bba611a365c73a5f7a3a6f4c67d7194f66
zhangyixing1007 <41513796+zhangyixing1007@users.noreply.github.com>
2020-01-31 18:48:20 +0800

    recursion, int[2], HashMap
    /*
    LeetCode Problem No.337:    https://leetcode.com/problems/house-robber-iii
    Author:                     zhangyixing1007
    Idea:                       recursion, int[2], HashMap
    Time:                       441 ms, beat 46.57%
    Space:                      36 MB, beat 97.70%
    */

--- 337_rob_3/Solution.java
+++ 337_rob_3/Solution.java
@@ -1,7 +1,7 @@
 /*
 LeetCode Problem No.337:    https://leetcode.com/problems/house-robber-iii
 Author:                     zhangyixing1007
-Idea:                       recursion, repeated calculation for rob(root.left.left) and rob(root.left.right), exponential extra cost
+Idea:                       recursion, int[2], HashMap
 Time:                       441 ms, beat 46.57%
 Space:                      36 MB, beat 97.70%
 */
@@ -17,29 +17,26 @@ Space:                      36 MB, beat 97.70%
 */
 
 class Solution {
-    
-    public int rob(TreeNode root){
-        if (root == null) return 0;
-        
-        int[] ans = new int[2];
-        int[] left = new int[2];
-        int[] right = new int[2];
-        
-        if (root.left!=null){
-            left[0] = rob(root.left.left) + rob(root.left.right);
-            left[1] = rob(root.left);   
+    public int rob(TreeNode root) {
+        if(root==null) return 0;
+        if (map.containsKey(root))
+            return map.get(root);
+        int[] ans=new int[2];
+        int[] left=new int[2];
+        int[] right=new int[2];
+        if(root.left!=null) {
+            left[0]=rob(root.left.left)+rob(root.left.right);
+            left[1]=rob(root.left);           
         }
-        
-        if (root.right!=null){
-            right[0] = rob(root.right.left) + rob(root.right.right);
-            right[1] = rob(root.right);   
+        if(root.right!=null) {
+            right[0]=rob(root.right.left)+rob(root.right.right);
+            right[1]=rob(root.right);   
         }
-        
-        
-        ans[0] = left[1]+right[1];
-        ans[1] = left[0]+right[0]+root.val;
-        
-        return Math.max(ans[0],ans[1]);
+        ans[0]=left[1]+right[1];
+        ans[1]=root.val+left[0]+right[0];
+        int value=Math.max(ans[0],ans[1]);
+        map.put(root,value);
+        return value;
     }
-        
+    HashMap<TreeNode, Integer> map=new HashMap<>();
 }
