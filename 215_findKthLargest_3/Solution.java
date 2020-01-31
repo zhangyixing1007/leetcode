edb50315a6ca5dc07f2643fd38fc928167b34678
zhangyixing1007 <41513796+zhangyixing1007@users.noreply.github.com>
2020-01-31 14:21:25 +0800

    quick sort
    /*
    LeetCode Problem No.215:    https://leetcode.com/problems/kth-largest-element-in-an-array/
    Author:                     zhangyixing1007
    Idea:                       quick sort
    Time:                       2 ms, beat 99.22%
    Space:                      32.7 MB, beat 94.63%
    */

--- 215_findKthLargest_3/Solution.java
+++ 215_findKthLargest_3/Solution.java
@@ -6,45 +6,32 @@ Time:                       2 ms, beat 99.22%
 Space:                      32.7 MB, beat 94.63%
 */
 
-class Solution
-{
-    public int findKthLargest(int[] nums, int k)
-    {
-        if (k<=0 || nums.length==0 || k > nums.length) return -1;
-        this.nums = nums;
-        return findPos(k-1, 0, nums.length-1);
+class Solution {
+    public int findKthLargest(int[] nums, int k) {
+        this.nums=nums;
+        return find(k-1, 0, nums.length-1);
     }
-    
-    private int[] nums;
-    
-    private void swap(int i, int j)
-    {
-        int tmp = nums[j];
-        nums[j] = nums[i];
-        nums[i] = tmp;        
+    int[] nums;
+    void swap(int i, int j){
+        int t=nums[i];
+        nums[i]=nums[j];
+        nums[j]=t;
     }
-    
-    private int findPos(int k, int left, int right)
-    {
-        if(left>right) return -1;
-        if(left == right) return nums[left];
-        
+    int find(int k, int left, int right){
+        if(left==right) return nums[left];
         Random random = new Random();
-        int mid = random.nextInt(right-left+1)+left;//right is not included here
+        int mid=left+random.nextInt(right-left+1);
         swap(mid,right);
-        int j = left;
-        for (int i = left; i < right; i++)
-        {
-            if(nums[i]>nums[right])
-            {
+        int j=left;
+        for(int i=left; i<right; i++){
+            if(nums[i]>nums[right]){
                 swap(i,j);
                 j++;
             }
         }
         swap(j,right);
-        
-        if (j == k) return nums[j];
-        else if (j < k) return findPos(k,j+1,right);
-        else return findPos(k,left,j-1);
+        if(j==k) return nums[k];
+        else if (j<k) return find(k, j+1, right);
+        else return find(k, left, j);
     }
 }
