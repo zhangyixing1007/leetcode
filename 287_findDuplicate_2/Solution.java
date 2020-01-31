4ee760f927f252e1aadfa3c764121da9f069069b
zhangyixing1007 <41513796+zhangyixing1007@users.noreply.github.com>
2020-01-31 16:24:09 +0800

    the place loop starts in a LinkedList
    /*
    LeetCode Problem No.287:    https://leetcode.com/problems/find-the-duplicate-number/
    Author:                     zhangyixing1007
    Idea:                       the place loop starts in a LinkedList
    Time:                       1 ms, beat 95.50%
    Space:                      37.2 MB, beat 95.29%
    */

--- 287_findDuplicate_2/Solution.java
+++ 287_findDuplicate_2/Solution.java
@@ -1,31 +1,23 @@
 /*
 LeetCode Problem No.287:    https://leetcode.com/problems/find-the-duplicate-number/
 Author:                     zhangyixing1007
-Idea:                       in a LinkedList, find the place that the loop starts
+Idea:                       the place loop starts in a LinkedList
 Time:                       1 ms, beat 95.50%
 Space:                      37.2 MB, beat 95.29%
 */
 
-class Solution
-{
-    public int findDuplicate(int[] nums)
-    {
-        if (nums.length==0) return -1;
-        int p1 = 0;
-        int p2 = 0;
-        do
-        {
-           p1 = nums[nums[p1]];
-           p2 = nums[p2];
-        }
-        while(p1!=p2);
-        p1 = 0;
-        do
-        {
-            p1 = nums[p1];
-            p2 = nums[p2];
-        }
-        while(p1!=p2);
+class Solution {
+    public int findDuplicate(int[] nums) {
+        int p1=0, p2=0;
+        do{
+            p1=nums[nums[p1]];
+            p2=nums[p2];
+        } while (p1!=p2);
+        p1=0;
+        do{
+            p1=nums[p1]; 
+            p2=nums[p2];
+        } while (p1!=p2);
         return p1;    
     }
 }
