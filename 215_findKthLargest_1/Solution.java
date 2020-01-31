343e7474971ae8ed2d54dd98e2e7d19bcdac0e88
zhangyixing1007 <41513796+zhangyixing1007@users.noreply.github.com>
2020-01-31 14:05:16 +0800

    Array.sort
    /*
    LeetCode Problem No.215:    https://leetcode.com/problems/kth-largest-element-in-an-array/
    Author:                     zhangyixing1007
    Idea:                       Array.sort
    Time:                       4 ms, beat 89.98%
    Space:                      95.89 MB, beat 94.67%
    */

--- 215_findKthLargest_1/Solution.java
+++ 215_findKthLargest_1/Solution.java
@@ -1,15 +1,13 @@
 /*
 LeetCode Problem No.215:    https://leetcode.com/problems/kth-largest-element-in-an-array/
 Author:                     zhangyixing1007
-Idea:                       Array.sort + return 
+Idea:                       Array.sort
 Time:                       4 ms, beat 89.98%
 Space:                      95.89 MB, beat 94.67%
 */
 
-class Solution
-{
-    public int findKthLargest(int[] nums, int k)
-    {
+class Solution{
+    public int findKthLargest(int[] nums, int k){
         Arrays.sort(nums);
         return nums[nums.length-k];
     }
