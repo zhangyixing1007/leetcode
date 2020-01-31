a63405d8e62cb31618c8eeb1b9f315a09b2d56ac
zhangyixing1007 <41513796+zhangyixing1007@users.noreply.github.com>
2020-01-31 15:40:20 +0800

    dynamic programming
    /*
    LeetCode Problem No.279:    https://leetcode.com/problems/perfect-squares/
    Author:                     zhangyixing1007
    Idea:                       dynamic programming
    Time:                       35 ms, beat 84.33%
    Space:                      35.4 MB, beat 41.69%
    */

--- 279_numSquares_1/Solution.java
+++ 279_numSquares_1/Solution.java
@@ -6,20 +6,17 @@ Time:                       35 ms, beat 84.33%
 Space:                      35.4 MB, beat 41.69%
 */
 
-class Solution
-{
-    public int numSquares(int n)
-    {
-        int[] ans = new int[n+1];
-        for (int i = 1; i<n+1; i++)
-        {
-            int min = Integer.MAX_VALUE;
-            for (int j = 1; j <= Math.sqrt(i); j++)
-            {
-                min = Math.min(min,ans[i-j*j]+1);
+class Solution {
+    public int numSquares(int n) {
+        if (n==0||n==1) return n;
+        int[] record=new int[n+1];
+        for (int i=1; i<n+1; i++){
+            record[i]=Integer.MAX_VALUE;
+            for (int j=1; j*j<=i; j++){
+                    int t=record[i-j*j]+1;
+                    record[i]=Math.min(record[i],t);                  
             }
-            ans[i]=min;
         }
-        return ans[n];
+        return record[n];
     }
 }
