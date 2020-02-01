e2e204631dcc78532a25db5f775df85135fffadc
zhangyixing1007 <41513796+zhangyixing1007@users.noreply.github.com>
2020-02-01 13:58:18 +0800

    bucket sort
    /*
    LeetCode Problem No.347:    https://leetcode.com/problems/top-k-frequent-elements/
    Author:                     zhangyixing1007
    Idea:                       bucket sort
    Time:                       15 ms, beat 99.49%
    Space:                      40.9 MB, beat 95.62%
    */

--- 347_topKFrequent_1/Solution.java
+++ 347_topKFrequent_1/Solution.java
@@ -8,26 +8,22 @@ Space:                      40.9 MB, beat 95.62%
 
 class Solution {
     public List<Integer> topKFrequent(int[] nums, int k) {
-        Map <Integer,Integer> map = new HashMap<>();
+        HashMap<Integer, Integer> map=new HashMap<>();
         for (int num:nums){
-            map.put(num,map.getOrDefault(num,0)+1);
+            map.put(num, map.getOrDefault(num,0)+1);
         }
-        
-        List[] list = new List[nums.length+1];
-        for (int num:map.keySet()){
-            int count = map.get(num);
-            if (list[count]==null){
-                list[count] = new ArrayList<Integer>();
-            }
-            list[count].add(num);
-            // System.out.println(list[count].toString());
+        List[] list=new List[nums.length+1];
+        for (int key:map.keySet()){
+            int count=map.get(key);
+            if(list[count]==null)
+                list[count]=new ArrayList<>();
+            list[count].add(key);
         }
-        
-        List<Integer> ans = new ArrayList<>();
-        for (int i = nums.length; k>0 ; i--){
-            if (list[i]!=null&& list[i].size()<=k){
+        List<Integer> ans=new ArrayList<>();
+        for (int i=list.length-1; i>-1; i--){
+            if(list[i]!=null&&list[i].size()<=k){
                 ans.addAll(list[i]);
-                k = k - list[i].size();
+                k-=list[i].size();
             }
         }
         return ans;
