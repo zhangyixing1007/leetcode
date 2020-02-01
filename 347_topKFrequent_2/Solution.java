699c175a2146f3b5709aa9a7cfc393023994c817
zhangyixing1007 <41513796+zhangyixing1007@users.noreply.github.com>
2020-02-01 14:08:35 +0800

    PriorityQueue
    /*
    LeetCode Problem No.347:    https://leetcode.com/problems/top-k-frequent-elements/
    Author:                     zhangyixing1007
    Idea:                       PriorityQueue
    Time:                       69 ms, beat 30.78%
    Space:                      40.6 MB, beat 96.12%
    */

--- 347_topKFrequent_2/Solution.java
+++ 347_topKFrequent_2/Solution.java
@@ -8,24 +8,19 @@ Space:                      40.6 MB, beat 96.12%
 
 class Solution {
     public List<Integer> topKFrequent(int[] nums, int k) {
-        Map <Integer,Integer> map = new HashMap<>();
+        HashMap<Integer, Integer> map=new HashMap<>();
         for (int num:nums){
-            map.put(num,map.getOrDefault(num,0)+1);
+            map.put(num, map.getOrDefault(num,0)+1);
         }
-        
-        PriorityQueue<Integer> q = new PriorityQueue<>((o1,o2) -> map.get(o1)-map.get(o2));
-        
-        for (int num:map.keySet()){
-            q.add(num);
-            if (q.size()>k)
-                q.poll();
+        Queue<Integer> q=new PriorityQueue<>((n1,n2)->map.get(n1)-map.get(n2));
+        for (int key:map.keySet()){
+            q.add(key);
+            if(q.size()>k) q.poll();
         }
-        
-        List<Integer> ans = new ArrayList<>();
-        while(!q.isEmpty()){
+        List<Integer> ans=new ArrayList<>();
+        while (!q.isEmpty()){
             ans.add(q.poll());
-        }
-        Collections.reverse(ans);
+        }        
         return ans;
     }
 }
