ad6109a2442ccb8cd8d7d6a0cb1657b7353a81b8
zhangyixing1007 <41513796+zhangyixing1007@users.noreply.github.com>
2020-02-01 13:23:50 +0800

    iteration, cancel the last "1" on the right
    /*
    LeetCode Problem No.338:    https://leetcode.com/problems/counting-bits
    Author:                     zhangyixing1007
    Idea:                       iteration, cancel the last "1" on the right
    Time:                       1 ms, beat 99.96%
    Space:                      38.4 MB, beat 86.52%
    */

--- 338_countBits_5/Solution.java
+++ 338_countBits_5/Solution.java
@@ -1,7 +1,7 @@
 /*
 LeetCode Problem No.338:    https://leetcode.com/problems/counting-bits
 Author:                     zhangyixing1007
-Idea:                       iteration, compare with the number without last "1" on the right, start from i = 1 instead of i = 0
+Idea:                       iteration, cancel the last "1" on the right
 Time:                       1 ms, beat 99.96%
 Space:                      38.4 MB, beat 86.52%
 */
