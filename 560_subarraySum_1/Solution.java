2ff39dedd0415d48dfb0a1c5f752b1eff17d8b6b
zhangyixing1007 <41513796+zhangyixing1007@users.noreply.github.com>
2020-02-02 14:33:59 +0800

    brute force
    /*
    LeetCode Problem No.560:    https://leetcode.com/problems/subarray-sum-equals-k/
    Author:                     zhangyixing1007
    Idea:                       brute force
    Time:                       150 ms, beat 40.08%
    Space:                      38.3 MB, beat 93.52%
    */

--- 560_subarraySum_1/Solution.java
+++ 560_subarraySum_1/Solution.java
@@ -12,8 +12,7 @@ class Solution {
         for (int i = 1; i < nums.length; i++){
             nums[i] += nums[i-1];
         }
-        int ans = 0;
-        
+        int ans = 0;       
         for (int i = 0; i < nums.length-1; i++){
             if (nums[i] == k) ans++;
             for (int j = i+1; j < nums.length; j++){
