462f3c82bbd305d73a2b5ad0aa8020fd9329a6ac
zhangyixing1007 <41513796+zhangyixing1007@users.noreply.github.com>
2020-02-02 15:07:12 +0800

    put most frequent one first
    /*
    LeetCode Problem No.621:    https://leetcode.com/problems/task-scheduler/
    Author:                     zhangyixing1007
    Idea:                       put most frequent one first
    Time:                       3 ms, beat 88.10%
    Space:                      38.2 MB, beat 93.89%
    */

--- 621_leastInterval/Solution.java
+++ 621_leastInterval/Solution.java
@@ -7,17 +7,13 @@ Space:                      38.2 MB, beat 93.89%
 */
 
 class Solution {
-    public int leastInterval(char[] tasks, int n) {   
-        int[] count = new int[26];
-        for (char c : tasks){
-            count[c-'A']++;
-        }
-        Arrays.sort(count);
-        int ans = (count[25]-1)*(n+1)+1;
-        int i = 24;
-        while(count[i] == count[25]){
-            ans++;i--;
-        }
+    public int leastInterval(char[] tasks, int n) {
+        int[] record=new int[26];
+        for(char c:tasks) record[c-'A']++;
+        Arrays.sort(record);
+        int ans=(record[25]-1)*(n+1)+1;
+        int i=24;
+        while(record[i--]==record[25]) ans++;
         return Math.max(ans,tasks.length);
     }
 }
