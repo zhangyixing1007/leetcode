9d3f6ba7232f9169da5f87e9740dcb55842ca831
zhangyixing1007 <41513796+zhangyixing1007@users.noreply.github.com>
2020-02-02 17:08:56 +0800

    topological sorting, based on Version 1
    /*
    LeetCode Problem No.207:    https://leetcode.com/problems/course-schedule/
    Author:                     zhangyixing1007
    Idea:                       topological sorting, based on Version 1 
    Time:                       40 ms, beat 52.1%
    Space:                      44.8 MB, beat 58.02%
    */

--- 207_canFinish_3/Solution.java
+++ 207_canFinish_3/Solution.java
@@ -22,7 +22,7 @@ class Solution{
             count[delete] = -1;
             for (int i = 0; i < prerequisites.length; i++){
                 if (prerequisites[i][0] == delete &&
-                    count[prerequisites[i][1]]!=0){
+                    count[prerequisites[i][1]]>0){
                     count[prerequisites[i][1]]--;                  
                 }
             }                     
