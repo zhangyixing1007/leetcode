f8479c9fa5f06f0e5cbacd4ca2edc7d33416de25
zhangyixing1007 <41513796+zhangyixing1007@users.noreply.github.com>
2020-09-27 15:21:49 +0800

    dfs, iteration
    LeetCode Problem No.236:    https://leetcode.com/problems/lowest-common-ancestor-of-a-binary-tree/
    Author:                     zhangyixing1007
    Idea:                       dfs, iteration
    Time:                       18 ms, beat 25.93%
    Space:                      38.1 MB, beat 21.67%

--- 236_lowestCommonAncestor_2/Solution.java
+++ 236_lowestCommonAncestor_2/Solution.java
@@ -1,7 +1,7 @@
 /*
 LeetCode Problem No.236:    https://leetcode.com/problems/lowest-common-ancestor-of-a-binary-tree/
 Author:                     zhangyixing1007
-Idea:                       dfs
+Idea:                       dfs, iteration
 Time:                       18 ms, beat 25.93%
 Space:                      38.1 MB, beat 21.67%
 */
@@ -16,31 +16,26 @@ Space:                      38.1 MB, beat 21.67%
  * }
  */
  
-class Solution 
-{
-    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) 
-    {
+class Solution {
+    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
         Map<TreeNode,TreeNode> map = new HashMap<>();
         Deque<TreeNode> stack = new ArrayDeque();
         
         map.put(root,null);
         stack.push(root);
-        while(!stack.isEmpty())
-        {
+        while(!stack.isEmpty()){
             TreeNode t = stack.pop();
             if (t.left!=null) {map.put(t.left,t);stack.push(t.left);}
             if (t.right!=null) {map.put(t.right,t);stack.push(t.right);}            
         }
         
         Set<TreeNode> set = new HashSet<>();
-        while (p!=null)
-        {
+        while (p!=null){
             set.add(p);
             p = map.get(p);
         }
         
-        while (!set.contains(q))
-        {
+        while (!set.contains(q)){
             q = map.get(q);
         }
         return q;
