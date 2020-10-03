f3662e30f0756d1f288d1816e02e318b57a9bbaf
zhangyixing1007 <41513796+zhangyixing1007@users.noreply.github.com>
2020-10-03 12:23:26 +0800

    save previous value in HashMap
    LeetCode Problem No.1:      https://leetcode.com/problems/two-sum/
    Author:                     zhangyixing1007
    Idea:                       save previous value in HashMap
    Time:                       2 ms, beat 99.58%
    Space:                      39.1MB, beat 42.80%

--- 001_twoSum/Solution.java
+++ 001_twoSum/Solution.java
@@ -1,24 +1,20 @@
 /*
 LeetCode Problem No.1:      https://leetcode.com/problems/two-sum/
 Author:                     zhangyixing1007
-Idea:                       loop once save values in HashMap, and check its complement
-Time:                       3 ms, beat 99.12%
-Space:                      37.8MB, beat 82.40%
+Idea:                       save previous value in HashMap
+Time:                       2 ms, beat 99.58%
+Space:                      39.1MB, beat 42.80%
 */
-
-import java.util.HashMap;
 
 class Solution {
     public int[] twoSum(int[] nums, int target) {
-    Map<Integer,Integer> map = new HashMap<Integer,Integer>();
-        for (int i = 0; i < nums.length; i ++){
-            int complement = target - nums[i];
-            if (map.containsKey(complement) && map.get(complement) != i){
-                return new int[] {map.get(complement),i};
+        Map<Integer, Integer> map=new HashMap<>();
+        for(int i=0; i<nums.length; i++){
+            if(map.containsKey(target-nums[i])){
+                return new int[]{map.get(target-nums[i]), i};
             }
-            map.put(nums[i],i);
+            map.put(nums[i], i);
         }
         return new int[]{};
     }
 }
- 
