01d0241250ccb2225ccf5d4ed62feada6e551dc7
zhangyixing1007 <41513796+zhangyixing1007@users.noreply.github.com>
2020-10-22 16:12:43 +0800

    count occurence
    LeetCode Problem No.0763:   https://link.zhihu.com/?target=https%3A//leetcode-cn.com/problems/partition-labels/
    Author:                     zhangyixing1007
    Idea:                       count occurence
    Time:                       10 ms, beat 28.46%
    Space:                      37.1 MB, beat 94.64%

---
+++ 0763_partitionLabels_2/Solution.java
@@ -0,0 +1,32 @@
+/*
+LeetCode Problem No.0763:   https://link.zhihu.com/?target=https%3A//leetcode-cn.com/problems/partition-labels/
+Author:                     zhangyixing1007
+Idea:                       count occurence
+Time:                       10 ms, beat 28.46%
+Space:                      37.1 MB, beat 94.64%
+*/
+
+class Solution {
+    public List<Integer> partitionLabels(String S) {
+        int[] count=new int[26];
+        for(char c:S.toCharArray()){
+            count[c-'a']++;
+        }
+        List<Integer> ans=new ArrayList<>();
+        Set<Integer> visited=new HashSet<>();
+        int left=0;
+        for(int i=0; i<S.length(); i++){
+            int k=S.charAt(i)-'a';
+            visited.add(k);
+            count[k]--;
+            if(count[k]==0){
+                visited.remove(k);
+            }
+            if(visited.isEmpty()){
+                ans.add(i-left+1);
+                left=i+1;
+            }
+        }
+        return ans;
+    }
+}
